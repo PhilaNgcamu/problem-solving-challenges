ceb01ccc8f000fbbe3335604c15d15de6d72297d
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-30 15:29:19 +0200

    Super Increasing

--- Day-4/superIncreasing.js
+++ Day-4/superIncreasing.js
@@ -1,14 +1,12 @@
 // For this challenge you will determine if each number is greater than the sum of all previous elements in an array.
 // have the function Superincreasing(arr) take the array of numbers stored in arr and determine if the array forms a super-increasing sequence where each element in the array is greater than the sum of all previous elements. The array will only consist of positive integers. For example: if arr is [1, 3, 6, 13, 54] then your program should return the string "true" because it forms a superincreasing sequence. If a superincreasing sequence isn't formed, then your program should return the string "false"
 function Superincreasing(arr) {
-  let boolean;
   let sum = 0;
   for (let i = 1; i < arr.length; i++) {
     sum += arr[i - 1];
-    if (arr[i] > sum) boolean = "true";
-    else return "false";
+    if (arr[i] < sum) return "false";
   }
-  return boolean;
+  return "true";
 }
 
 console.log(Superincreasing([1, 2, 5, 10]));
