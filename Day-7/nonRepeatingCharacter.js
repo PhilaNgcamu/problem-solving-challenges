3924debc38828d2f34882f897eee3f69ceb39ac8
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-06 15:35:09 +0200

    None Repeating Letters

--- Day-7/nonRepeatingCharacter.js
+++ Day-7/nonRepeatingCharacter.js
@@ -2,7 +2,17 @@
 // have the function NonrepeatingCharacter(str) take the str parameter being passed, which will contain only alphabetic characters and spaces, and return the first non-repeating character. For example: if str is "agettkgaeee" then your program should return k. The string will always contain at least one character and there will always be at least one non-repeating character.
 
 function NonrepeatingCharacter(str) {
-  return str;
+  let array = str.split("");
+  const letters = {};
+
+  for (let i = 0; i < array.length; i++) {
+    if (letters.hasOwnProperty(array[i])) {
+      letters[array[i]]++;
+    } else letters[array[i]] = 1;
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (letters[array[i]] === 1) return array[i];
+  }
 }
 
 console.log(NonrepeatingCharacter("agettkgaeee"));
