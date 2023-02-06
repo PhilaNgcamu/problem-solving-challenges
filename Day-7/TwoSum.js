20951d8e6aaf3c7c715ee97b0151714246e2d7cc
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-06 15:56:55 +0200

    Two Sum

--- Day-7/TwoSum.js
+++ Day-7/TwoSum.js
@@ -2,7 +2,15 @@
 // have the function TwoSum(arr) take the array of integers stored in arr, and determine if any two numbers (excluding the first element) in the array can sum up to the first element in the array. For example: if arr is [7, 3, 5, 2, -4, 8, 11], then there are actually two pairs that sum to the number 7: [5, 2] and [-4, 11]. Your program should return all pairs, with the numbers separated by a comma, in the order the first number appears in the array. Pairs should be separated by a space. So for the example above, your program would return: 5,2 -4,11
 
 function TwoSum(arr) {
-  return arr;
+  const first = arr[0];
+  const array = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] + arr[i] === first) array.push([arr[i], arr[j]]);
+    }
+  }
+  return array.join(" ");
 }
 
-console.log(TwoSum([7, 3, 5, 2, -4, 8, 11]));
+console.log(TwoSum([17, 4, 5, 6, 10, 11, 4, -3, -5, 3, 15, 2, 7]));
