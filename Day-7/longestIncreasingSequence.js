414ca73806abe09787276572f1f8ad5c31bf5520
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-06 20:14:43 +0200

    Longest Increasing Sequence

--- Day-7/longestIncreasingSequence.js
+++ Day-7/longestIncreasingSequence.js
@@ -1,18 +1,19 @@
 // For this challenge you will compute the length of the longest increasing subsequence.
 // have the function LongestIncreasingSequence(arr) take the array of positive integers stored in arr and return the length of the longest increasing subsequence (LIS). A LIS is a subset of the original list where the numbers are in sorted order, from lowest to highest, and are in increasing order. The sequence does not need to be contiguous or unique, and there can be several different subsequences. For example: if arr is [4, 3, 5, 1, 6] then a possible LIS is[3, 5, 6], and another is[1, 6].For this input, your program should return 3 because that is the length of the longest increasing subsequence.
 function LongestIncreasingSequence(arr) {
-  let lis;
-  let length = 1;
-  let difference;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      difference = arr[j];
+  const dp = Array(arr.length).fill(1);
+  let maxLength = 1;
 
-      if (arr[j] > arr[i]) {
-        length++;
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j]) {
+        console.log(dp[j]);
+        dp[i] = Math.max(dp[i], dp[j] + 1);
       }
     }
+    maxLength = Math.max(maxLength, dp[i]);
   }
-  return length;
+
+  return maxLength;
 }
 console.log(LongestIncreasingSequence([4, 3, 5, 1, 6]));
