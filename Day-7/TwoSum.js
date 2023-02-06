adef2559a165f5f88c062e1142c192630493fd75
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-06 15:42:21 +0200

    Two Sum

---
+++ Day-7/TwoSum.js
@@ -0,0 +1,8 @@
+// For this challenge you will determine if two elements can sum to some larger number.
+// have the function TwoSum(arr) take the array of integers stored in arr, and determine if any two numbers (excluding the first element) in the array can sum up to the first element in the array. For example: if arr is [7, 3, 5, 2, -4, 8, 11], then there are actually two pairs that sum to the number 7: [5, 2] and [-4, 11]. Your program should return all pairs, with the numbers separated by a comma, in the order the first number appears in the array. Pairs should be separated by a space. So for the example above, your program would return: 5,2 -4,11
+
+function TwoSum(arr) {
+  return arr;
+}
+
+console.log(TwoSum([7, 3, 5, 2, -4, 8, 11]));
