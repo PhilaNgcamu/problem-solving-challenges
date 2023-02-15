b87a06999d8e43f392f60f17c78d1526b7917901
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-15 21:14:34 +0200

    @d Array

--- Day-8/2dArray.js
+++ Day-8/2dArray.js
@@ -8,3 +8,8 @@ for (let i = 0; i < arr.length; i++) {
   //print out: 1, 4, 7
   console.log(arr[i][0]);
 }
+
+for (let i = 0; i < arr.length; i++) {
+  //print out: 3, 6, 9
+  console.log(arr[i][arr[i].length - 1]);
+}
