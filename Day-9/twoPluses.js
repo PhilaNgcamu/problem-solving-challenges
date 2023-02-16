028b5b8bda01ada56c7a19c3a277030b08940ae5
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-16 12:33:13 +0200

    Still solving two pluses

--- Day-9/twoPluses.js
+++ Day-9/twoPluses.js
@@ -3,10 +3,35 @@
 // In the diagram below, the blue pluses are valid and the orange ones are not valid.
 
 const array = [
-  ["BGBBGB"],
-  ["GGGGGG"],
-  ["BGBBGB"],
-  ["GGGGGG"],
-  ["BGBBGB"],
-  ["BGBBGB"],
+  ["B", "G", "B", "B", "G", "B"],
+  ["G", "G", "G", "G", "G", "G"],
+  ["B", "G", "B", "B", "G", "B"],
+  ["G", "G", "G", "G", "G", "G"],
+  ["B", "G", "B", "B", "G", "B"],
+  ["B", "G", "B", "B", "G", "B"],
 ];
+
+//valid pluses
+
+console.log(array[3][0]);
+console.log(array[3][1]);
+console.log(array[3][2]);
+console.log(array[3][3]);
+console.log(array[3][4]);
+console.log(array[3][5]);
+console.log(array[4][1]);
+console.log(array[4][4]);
+console.log(array[5][1]);
+console.log(array[5][4]);
+
+console.log(array[0][1]); //1
+console.log(array[1][0]); //1
+console.log(array[1][1]); //1
+console.log(array[1][2]); //1
+console.log(array[2][1]); //1
+
+console.log(array[0][4]); //2
+console.log(array[1][3]); //2
+console.log(array[1][4]); //2
+console.log(array[1][5]); //2
+console.log(array[2][4]); //2
