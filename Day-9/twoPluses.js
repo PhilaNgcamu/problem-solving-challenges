b40876fb3e8e7f1858e598a93733e1d845ae6152
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-16 11:55:13 +0200

    Two Pluses

---
+++ Day-9/twoPluses.js
@@ -0,0 +1,14 @@
+//Hackerrank - A valid plus is defined here as the crossing of two segments (horizontal and vertical) of equal lengths. These lengths must be odd, and the middle cell of its horizontal segment must cross the middle cell of its vertical segment.
+
+// In the diagram below, the blue pluses are valid and the orange ones are not valid.
+
+const array = [
+  ["BGBBGB"],
+  ["GGGGGG"],
+  ["BGBBGB"],
+  ["GGGGGG"],
+  ["BGBBGB"],
+  ["BGBBGB"],
+];
+
+console.log();
