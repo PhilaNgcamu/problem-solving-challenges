b55efc3760f935bd208f414b5aab6e677cd80f75
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-17 09:17:58 +0200

    Kangaroo

--- Day-10/kangaroo.js
+++ Day-10/kangaroo.js
@@ -1,7 +1,8 @@
 //Hackerrank - Implementation > Number Line
 function kangaroo(x1, v1, x2, v2) {
   // Write your code here
-
+  let sum1 = x1;
+  let sum2 = x2;
   for (let i = 0; i <= 10000; i++) {
     sum1 += v1;
     sum2 += v2;
