97cf13ff7e584453cb713b6ad9819c7e3a26d23c
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-17 08:41:41 +0200

    Migratory Birds

--- Day-9/migratoryBirds.js
+++ Day-9/migratoryBirds.js
@@ -2,9 +2,19 @@ function migratoryBirds(arr) {
   // Write your code here
   let obj = {};
   for (let i = 0; i < arr.length; i++) {
-    if (obj.hasOwnProperty(arr[i])) return arr[i]++;
-    else arr[i] = 0;
+    if (obj.hasOwnProperty(arr[i])) obj[arr[i]]++;
+    else obj[arr[i]] = 1;
   }
-  // return obj[1]
+
+  let maxNumber = 0;
+  let count = 0;
+  for (let number in obj) {
+    if (obj[number] > count) {
+      count = obj[number];
+      maxNumber = number;
+    }
+  }
+  return maxNumber;
 }
+
 console.log(migratoryBirds([1, 4, 4, 4, 5, 3]));
