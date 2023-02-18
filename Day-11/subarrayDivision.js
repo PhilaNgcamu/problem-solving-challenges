ae0c5b4c9555ef687c32e66c29cdb3348d4a912d
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-18 14:05:39 +0200

    still solving the subarray division

--- Day-11/subarrayDivision.js
+++ Day-11/subarrayDivision.js
@@ -2,10 +2,17 @@ function birthday(s, d, m) {
   // squares of the bars(s) = array of integers(d)
   // segment - squares (m) = length = Ron's birth month
   // sum of the integers in the array(squares) = Ron's birth day (d) segment
-  if (s.length === 1 && s[0] === m) return m;
   let count = 0;
-  for (let j = 0; j < s.length; j += 2) {
-    if (s[j] + s[j + 1] === d || s[j] + s[j - 1] === d) count++;
+  let sub = [];
+  for (let j = 0; j < s.length; j++) {
+    sub.push(s[j]);
+    for (let i = j; i < s.length; i++) {
+      sub.push(s[i]);
+      if (sub.length === m && sub.reduce((a, b) => a + b, 0) === d) {
+        count++;
+        sub = [];
+      }
+    }
   }
   return count;
 }
