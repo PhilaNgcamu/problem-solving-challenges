a7febc4d533ceee11ab2cf8be82f199f7f62c7de
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-18 14:18:11 +0200

    Still solving Subarray

--- Day-11/subarrayDivision.js
+++ Day-11/subarrayDivision.js
@@ -3,16 +3,11 @@ function birthday(s, d, m) {
   // segment - squares (m) = length = Ron's birth month
   // sum of the integers in the array(squares) = Ron's birth day (d) segment
   let count = 0;
-  let sub = [];
+  let sub = s;
   for (let j = 0; j < s.length; j++) {
-    sub.push(s[j]);
-    for (let i = j; i < s.length; i++) {
-      sub.push(s[i]);
-      if (sub.length === m && sub.reduce((a, b) => a + b, 0) === d) {
-        count++;
-        sub = [];
-      }
-    }
+    sub = sub.slice(j, m + j);
+    if (sub.reduce((a, b) => a + b, 0) === d) count++;
+    sub = [];
   }
   return count;
 }
