fe1b4a90e04cf7067a3360bc799b98e95295d9c0
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 18:47:32 +0200

    Cats and A Mouse

--- Day-12/catsAndAMouse.js
+++ Day-12/catsAndAMouse.js
@@ -1,6 +1,8 @@
 // Hackerrank > Cats And A Mouse
 function catAndMouse(x, y, z) {
-  return x;
+  let catA = Math.abs(z - x);
+  let catB = Math.abs(z - y);
+  return catB > catA ? "Cat A" : catB < catA ? "Cat B" : "Mouse C";
 }
 
 console.log(catAndMouse(2, 5, 4));
