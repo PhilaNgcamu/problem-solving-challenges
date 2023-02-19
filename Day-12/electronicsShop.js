57f0ba8560357a3db71a20ce9475a6a22bf3408e
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 18:24:05 +0200

    Electronics Shop

--- Day-12/electronicsShop.js
+++ Day-12/electronicsShop.js
@@ -1,7 +1,17 @@
+// Hackerrank > Electronics Shop
 function getMoneySpent(keyboards, drives, b) {
-  /*
-   * Write your code here.
-   */
+  // compare the keyboard price with the usb price
+  let array = [];
+  for (let i = 0; i < keyboards.length; i++) {
+    let keyboard = keyboards[i];
+    for (let j = 0; j < drives.length; j++) {
+      // -- if the price of both <= 60, store in an array
+      if (keyboard + drives[j] <= b) array.push(keyboard + drives[j]);
+    }
+  }
+  return !array.length ? "-1" : Math.max(...array);
+
+  // --- return the max cost in the array else -1
 }
 
-console.log(getMoneySpent());
+console.log(getMoneySpent([3, 1], [5, 2, 8], 10));
