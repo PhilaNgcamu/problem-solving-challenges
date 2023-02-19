e7d6f438ee3f6c593bb1118323c180a068761c3a
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 15:15:36 +0200

    Drawing Book

--- Day-12/drawingBook.js
+++ Day-12/drawingBook.js
@@ -1,1 +1,25 @@
 // Hackerrank > Drawing Book
+
+function pageCount(n, p) {
+  // n = length of book
+  // p = page of the book desired
+  let countFront = 0;
+  let countBack = 0;
+
+  // use a for loop to count to a page
+  for (let i = 1; i <= n; i++) {
+    // -- if a page === i then Math.floor(i/2)
+    if (p === i) {
+      countFront += Math.floor(i / 2);
+    }
+  }
+  // use a for loop to count to a page
+  for (let i = 100; i <= 0; i--) {
+    // -- if a page === i then Math.floor(i/2)
+    if (p === i) {
+      countBack += Math.floor(i / 2);
+    }
+  }
+  if (countBack > countFront) return countBack;
+  return countFront;
+}
