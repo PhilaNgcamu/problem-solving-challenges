6a2dc683206066d8b0c5544add62dcdb9f979792
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 16:43:09 +0200

    Counting Valleys

--- Day-12/countingValleys.js
+++ Day-12/countingValleys.js
@@ -2,10 +2,15 @@
 function countingValleys(steps, path) {
   let count = 0;
   let valley = 0;
-  for (let i = 0; i < 8; i++) {
-    if ("U" === path[i]) count++;
-    else count--;
-    //DDUUDDUDUUUD
+  //use for loop for each step
+  for (let i = 0; i < steps; i++) {
+    //if count is 0 and the step is downhill then valley++
+    if (count === 0 && "D" === path[i]) {
+      count--;
+      valley++;
+    } else if ("D" === path[i]) {
+      count--;
+    } else count++;
   }
   return valley;
 }
