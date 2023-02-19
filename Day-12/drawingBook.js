3635ca7d062821cf55752e235c301867497697c7
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 15:51:26 +0200

    Drawing Book

--- Day-12/drawingBook.js
+++ Day-12/drawingBook.js
@@ -3,15 +3,10 @@
 function pageCount(n, p) {
   // n = length of book
   // p = page of the book desired
-  let countFront = 0;
-  let countBack = 0;
+  const frontPages = Math.floor(p / 2);
+  const backPages = Math.floor(n / 2 - frontPages);
 
-  if (n - p === 1 || n - p === n - 1) return 0;
-
-  countFront = Math.floor(p / 2);
-  countBack = Math.floor((n - p) / 2);
-  if (countBack >= countFront) return countFront;
-  return countBack;
+  return Math.min(frontPages, backPages);
 }
 
 console.log(pageCount(101, 92));
