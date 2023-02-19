35a1aca25721938fdfb920e747817e7afd046ef1
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 15:33:33 +0200

    Solving The Drawing Book

--- Day-12/drawingBook.js
+++ Day-12/drawingBook.js
@@ -8,16 +8,10 @@ function pageCount(n, p) {
 
   if (n - p === 1 || n - p === n - 1) return 0;
 
-  // use a for loop to count to a page
-  for (let i = 1; i <= n; i++) {
-    // -- if a page === i then Math.floor(i/2)
-    if (p === i) {
-      countFront += Math.floor(i / 2);
-    }
-  }
+  countFront = Math.floor(p / 2);
   countBack = Math.floor((n - p) / 2);
   if (countBack >= countFront) return countFront;
   return countBack;
 }
 
-console.log(pageCount(100, 92));
+console.log(pageCount(101, 92));
