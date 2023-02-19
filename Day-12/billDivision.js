6862f8ddd1da3fb1c6b01b83f95fafadab8a1ca8
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 11:04:13 +0200

    Bill Division

--- Day-12/billDivision.js
+++ Day-12/billDivision.js
@@ -6,8 +6,8 @@ function bonAppetit(bill, k, b) {
 
   // find the difference between the bill contributed and bill of Anna
   let difference = b - Math.floor(array.reduce((a, b) => a + b) / 2);
-  return difference;
   // if the difference > 0, print the difference else print "Bon Appetit"
+  console.log(difference > 0 ? difference : "Bon Appetit");
 }
 
-console.log(bonAppetit([3, 10, 2, 9], 1, 12));
+bonAppetit([3, 10, 2, 9], 1, 12);
