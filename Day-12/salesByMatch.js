c63c12cabff87b9dfa5bb382b866e2691990e3b5
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-19 14:50:24 +0200

    Still Solving the Match By Sales

--- Day-12/salesByMatch.js
+++ Day-12/salesByMatch.js
@@ -1,22 +1,15 @@
 function sockMerchant(ar, n) {
   // arr[i] = color of each sock
-  // compare each sock with another sock
   let count = 0;
-  let array = [...ar];
+  let socks = {};
 
-  for (let i = 0; i < array.length - 1; i++) {
-    let socks = [];
-    let sock = array[i];
-    for (let j = 0; j < array.length - 1; j++) {
-      if (sock === array[j]) {
-        socks.push(sock);
-        array.splice(j, 1);
-      }
-    }
-    count += Math.floor(socks.length / 2);
-    console.log(socks);
+  for (let i = 0; i < n; i++) {
+    //store socks
+    if (socks.hasOwnProperty(ar[i])) {
+      socks[ar[i]]++;
+    } else socks[ar[i]] = 1;
   }
-  return count;
+  return socks;
 }
 
 console.log(sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20], 9));
