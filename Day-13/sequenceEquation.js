a17138fed0be0fcca9ebae5a976fb537dc20c421
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 23:21:17 +0200

    Sequence equation

--- Day-13/sequenceEquation.js
+++ Day-13/sequenceEquation.js
@@ -2,9 +2,15 @@ function permutationEquation(p) {
   //let i = number find the number in the array by index i
   //then push the index
   let array = [];
-  for (let i = 1; i <= p.length; i++) {
-    array.push(p.indexOf(i));
+  for (let i = 1; i < p.length + 1; i++) {
+    array.push(p.indexOf(i) + 1);
+    //p[p[y]]
   }
-  return array;
+  let output = [];
+  console.log(p);
+  for (let i of array) {
+    output.push(p.indexOf(i) + 1);
+  }
+  return output;
 }
 console.log(permutationEquation([5, 2, 1, 3, 4]));
