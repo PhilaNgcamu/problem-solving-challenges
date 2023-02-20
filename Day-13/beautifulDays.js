6f757a278584c1f25bb02d935ea2e532ce86bb5f
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 10:21:33 +0200

    Solving Beautiful Days

--- Day-13/beautifulDays.js
+++ Day-13/beautifulDays.js
@@ -3,9 +3,10 @@ function beautifulDays(i, j, k) {
   let count = 0;
   for (let s = i; s <= j; s++) {
     let num = Number(s.toString().split("").reverse().join(""));
+    console.log(num);
     if (Math.abs(i - num) % k === 0) count++;
   }
   return count++;
 }
 
-console.log(beautifulDays(201, 205, 6));
+console.log(beautifulDays(201, 205, 11));
