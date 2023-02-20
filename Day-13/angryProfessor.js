eedf1c3423bb8981061f81a9dea68a9c2348bf8b
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 10:06:29 +0200

    Angry Professor

--- Day-13/angryProfessor.js
+++ Day-13/angryProfessor.js
@@ -1,8 +1,12 @@
 function angryProfessor(k, a) {
   // a.length === number of students
   // k === arrived on time
-  // return "YES" if k < a.length else "NO"
-  return k >= a.length - k ? "YES" : "NO";
+  // return "YES" if k has been met else "NO"
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] <= 0) count++;
+  }
+  return count >= k ? "NO" : "YES";
 }
 
 console.log(angryProfessor(3, [-1, -3, 4, 2]));
