ecdf084631c3cb0f0f586566c23439378701b119
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 09:01:39 +0200

    Solving The Designer PDF Viewer

---
+++ Day-13/designerPdfViewer.js
@@ -0,0 +1,14 @@
+// Hackerrank > Designer PDF Viewer
+function designerPdfViewer(h, word) {
+  // Write your code here
+}
+
+console.log(
+  designerPdfViewer(
+    [
+      1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+      5,
+    ],
+    "abc"
+  )
+);
