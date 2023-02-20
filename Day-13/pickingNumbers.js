ccfd38cde15229f21fd842ef4b05150b5e23a49d
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 08:14:50 +0200

    Still Solving Picking Numbers

--- Day-13/pickingNumbers.js
+++ Day-13/pickingNumbers.js
@@ -1,19 +1,19 @@
 // Hackerrank > Picking Numbers
 function pickingNumbers(a) {
-  // find the subarrays that are two elements <= 1; and store them in an array
-  const array = [];
-  for (let i = 0; i < a.length; i++) {
-    const arr = [];
-    arr.push(a[i]);
-    for (let j = 0; j < a.length; j++) {
-      if (i === j) continue;
-      if (Math.abs(a[i] - a[j]) <= 1) {
-        arr.push(a[j]);
+  // find the subarrays that are two elements <= 1;
+  let maxCount = 0;
+  let array = a.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i++) {
+    let currentCount = 1;
+    for (let j = 0 + 1; j < array.length; j++) {
+      if (i > 0) {
+      }
+      if (Math.abs(array[i] - array[j]) <= 1) {
       }
     }
-    array.push(arr);
   }
-  return array.sort((a, b) => a.length - b.length)[array.length - 1].length;
+  return array;
+
   // then find the longest array and return its length
 }
-console.log(pickingNumbers([4, 6, 5, 3, 3, 1]));
+console.log(pickingNumbers([1, 1, 2, 2, 4, 4, 5, 5, 5]));
