11a1e43b0eb3d5e1c8f4fc39714f0ff58bd09023
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 16:10:45 +0200

    Save The Prisoner!

--- Day-13/saveThePrisoner.js
+++ Day-13/saveThePrisoner.js
@@ -1,15 +1,12 @@
 // Hackerrank > Save The Prisoner!
 function saveThePrisoner(n, m, s) {
-  // Write your code here
-  // use the number of candies (m) to loop around prisoners
-  let array = new Array(n).fill(1);
-  let index = 0;
-  for (let i = 0; i < m; i++) {
-    if (array.indexOf(array[array.length - 1]) === i) {
-      array.fill(3, i);
-    }
-  }
-  return array;
+  //the warned prisoner is s + m - 1
+  //then distrubute the remaining sweets by modulo n
+  //I.e sweets % number of prisoners
+  let output = (s + m - 1) % n;
+  console.log(output);
+  if (output === 0) return n;
+  return output;
 }
 
 console.log(saveThePrisoner(4, 6, 2));
