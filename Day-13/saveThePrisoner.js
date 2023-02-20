d1ee7dc40ef73eb9d691f71765283fa24bc3b7ac
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 15:17:43 +0200

    Solving Save The Prisoner

---
+++ Day-13/saveThePrisoner.js
@@ -0,0 +1,6 @@
+// Hackerrank > Save The Prisoner!
+function saveThePrisoner(n, m, s) {
+  // Write your code here
+  // use the number of candies (m) to loop around prisoners
+  for (let i = 0; i < m; i++) {}
+}
