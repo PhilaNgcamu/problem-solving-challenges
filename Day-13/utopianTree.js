01d6b93e53dde1a26e0c82af69ab57f41b26bb04
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-20 09:40:54 +0200

    Utopian Tree

--- Day-13/utopianTree.js
+++ Day-13/utopianTree.js
@@ -1,4 +1,12 @@
 function utopianTree(n) {
-  // Write your code here
+  // use a for loop where i % 2 === 0 === "spring" else "summer"
+  // if spring, maxHeight * 2 else maxHeight + 1
+  // return maxHeight
+  let maxHeight = 1;
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) maxHeight *= 2;
+    else maxHeight += 1;
+  }
+  return maxHeight;
 }
-console.log(utopianTree(0));
+console.log(utopianTree(7));
