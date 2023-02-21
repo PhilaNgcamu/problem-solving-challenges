e2240258750d824db5320424f6a901d3604d782b
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-21 08:28:35 +0200

    Find Digits

--- Day-14/findDigits.js
+++ Day-14/findDigits.js
@@ -1,4 +1,10 @@
+// Hackerrank > Find Digits
 function findDigits(n) {
-  return 124;
+  let numbers = n.toString().split("");
+  let count = 0;
+  for (let i = 0; i <= numbers.length; i++) {
+    if (n % Number(numbers[i]) === 0) count++;
+  }
+  return count;
 }
-console.log(findDigits());
+console.log(findDigits(124));
