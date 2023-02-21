b29d3b63f944e3446e83bc6cd53f832335f821d4
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-21 10:53:14 +0200

    Solving Sherlock and Squares

--- Day-14/sherlockAndSquares.js
+++ Day-14/sherlockAndSquares.js
@@ -1,4 +1,11 @@
+// Hackerrank > Sherlock and Squares
 function squares(a, b) {
-  return a;
+  let count = 0;
+  let square;
+  for (let i = 1; i <= b; i++) {
+    square = i * i;
+    if (square >= a && square <= b) count++;
+  }
+  return count;
 }
 console.log(squares(24, 49));
