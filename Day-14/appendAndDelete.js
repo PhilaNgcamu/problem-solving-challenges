72b78b5bd16c0f9ab14e2dbe90b568b1eb058221
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-21 10:35:13 +0200

    Append And Delete

--- Day-14/appendAndDelete.js
+++ Day-14/appendAndDelete.js
@@ -1,7 +1,26 @@
+// Hackerrank > Append And Deleted
 function appendAndDelete(s, t, k) {
-  let word = s.slice(0, s.indexOf(s.slice(-Math.ceil(k / 2))));
-  k = k - Math.ceil(k / 2);
-  let second = t.slice(-k);
-  return t === word + second ? "Yes" : "No";
+  let firstWord = s.split("");
+  let secondWord = t.split("");
+  let operations = k;
+
+  console.log(firstWord + "\n" + secondWord);
+
+  if (k >= firstWord.length + secondWord.length) return "Yes";
+
+  let cut = Math.min(firstWord.length, secondWord.length);
+  let min = Math.min(firstWord.length, secondWord.length);
+
+  for (let i = 0; i < min; i++) {
+    if (firstWord[i] !== secondWord[i]) {
+      cut = i;
+      break;
+    }
+  }
+
+  operations -= firstWord.length - cut + (secondWord.length - cut);
+
+  if (operations >= 0 && operations % 2 === 0) return "Yes";
+  return "No";
 }
-console.log(appendAndDelete("hackerhappy", "hackerrank", 9));
+console.log(appendAndDelete("hackerhappy", "hackerrank", 11));
