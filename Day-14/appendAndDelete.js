f06c1ab2c247ee069303fe7bd6d77d70e9b22707
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-21 09:47:41 +0200

    Solving Append And Delete

--- Day-14/appendAndDelete.js
+++ Day-14/appendAndDelete.js
@@ -1,4 +1,7 @@
 function appendAndDelete(s, t, k) {
-  return k;
+  s.slice(-Math.ceil(k / 2));
+  k = k - Math.ceil(k / 2);
+  t.slice(-k);
+  return s.slice(0, 6) + t.slice(-k);
 }
 console.log(appendAndDelete("hackerhappy", "hackerrank", 9));
