b79e45d19ec3e654b5948ab2a5b301597163c196
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-22 08:10:33 +0200

    Cut The Sticks

--- Day-14/cutTheSticks.js
+++ Day-14/cutTheSticks.js
@@ -4,17 +4,14 @@ function cutTheSticks(arr) {
   let max = Math.max(...arr);
 
   //Declare out array
-  arr.sort((a, b) => a - b);
-  let a = new Array(arr.length + 1).fill([]);
+  let a = new Array(arr.length).fill([]);
   a[0] = arr;
-  let i = 0;
-  while (i < max) {
-    arr[i] = arr[i] - shortest;
-    a[i + 1].push(arr[i]);
-    i++;
+  for (let i = 0; i < max; i++) {
+    shortest = Math.min(...a[i]);
+    for (let j = 0; j < max; j++) {
+      a[i + 1].push(arr[j] - shortest);
+    }
+    return a;
   }
-  return a;
-
-  //return array;
 }
 console.log(cutTheSticks([1, 2, 3]));
