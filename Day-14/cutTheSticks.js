ade5a8e11bf21bf151638fb10ada5f45f0b1b5cb
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-22 07:48:13 +0200

    Solving Cut the Sticks

--- Day-14/cutTheSticks.js
+++ Day-14/cutTheSticks.js
@@ -1,18 +1,20 @@
 function cutTheSticks(arr) {
-  // // determine the shortest stick
-  // let shortest = Math.min(...arr);
-  // let max = Math.max(...arr);
-  // //Declare out array
-  // arr.sort((a, b) => a - b);
-  // let a = new Array(arr.length + 1).fill([]);
-  // a[0] = arr;
-  // let i = 0;
-  // while (i < max) {
-  //   arr[i] = arr[i] - shortest;
-  //   a[i + 1].push(arr[i]);
-  //   i++;
-  // }
-  // return a;
+  // determine the shortest stick
+  let shortest = Math.min(...arr);
+  let max = Math.max(...arr);
+
+  //Declare out array
+  arr.sort((a, b) => a - b);
+  let a = new Array(arr.length + 1).fill([]);
+  a[0] = arr;
+  let i = 0;
+  while (i < max) {
+    arr[i] = arr[i] - shortest;
+    a[i + 1].push(arr[i]);
+    i++;
+  }
+  return a;
+
   //return array;
 }
 console.log(cutTheSticks([1, 2, 3]));
