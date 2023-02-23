bba219fe9162fa008fcb50f78264ed3b02576dd8
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-23 08:10:49 +0200

    Recursion

--- Day-16-Recursion/recursion.js
+++ Day-16-Recursion/recursion.js
@@ -1,12 +1,21 @@
-let countDown = function f(n) {
-  let nextNumber = n - 1;
-  if (nextNumber === 1) {
-    console.log(nextNumber);
-  } else {
-    console.log(nextNumber);
-    countDown(nextNumber);
-  }
-};
-let newYearCountDown = countDown;
-countDown = null;
-newYearCountDown(10);
+// let countDown = function f(n) {
+//   let nextNumber = n - 1;
+//   if (nextNumber === 1) {
+//     console.log(nextNumber);
+//   } else {
+//     console.log(nextNumber);
+//     countDown(nextNumber);
+//   }
+// };
+// let newYearCountDown = countDown;
+// countDown = null;
+// newYearCountDown(10);
+
+// 10 + 9 + 8 + ... + 1 = sum
+// n - 1 if n === 1
+function sum(n) {
+  let nextNumber = n;
+  if (nextNumber === 1) return nextNumber;
+  else return n + sum(n - 1);
+}
+console.log(sum(10));
