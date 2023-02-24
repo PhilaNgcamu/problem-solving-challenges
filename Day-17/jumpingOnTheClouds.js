657048c3b73add7ce35bf76efb6807554a156e4f
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-24 12:08:44 +0200

    Jumping On The Clouds

--- Day-17/jumpingOnTheClouds.js
+++ Day-17/jumpingOnTheClouds.js
@@ -6,9 +6,11 @@ function jumpingOnClouds(c) {
     //if the player is at cloud 1 then jump to the next the same counter
     if (c[i] === 1) {
       continue;
-    } else if (c[i + 1] === 0) counter += 2;
-    else if (c[i] === 0 && c[i + 1] === 1) {
-      continue;
+    } else if (c[i + 1] === 0) {
+      i += 1;
+      counter++;
+    } else if (c[i] === 0) {
+      counter++;
     }
   }
   return counter;
