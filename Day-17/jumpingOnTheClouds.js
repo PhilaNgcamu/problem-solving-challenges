d5e692df3d60f6602a796c20918613b5b3a867aa
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-24 12:29:28 +0200

    Solving Jumping On The Clouds

--- Day-17/jumpingOnTheClouds.js
+++ Day-17/jumpingOnTheClouds.js
@@ -4,12 +4,11 @@ function jumpingOnClouds(c) {
   // loop through the array
   for (let i = 0; i < c.length; i++) {
     //if the player is at cloud 1 then jump to the next the same counter
-    if (c[i] === 1) {
-      continue;
-    } else if (c[i + 1] === 0) {
+    //0 0 1 0 0 1 0
+    if (c[i] === 0 && c[i + 1] === 1) {
       i += 1;
       counter++;
-    } else if (c[i] === 0) {
+    } else if (c[i] === 0 && c[i + 1] === 0) {
       counter++;
     }
   }
