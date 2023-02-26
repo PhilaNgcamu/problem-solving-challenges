fc9c85b6eae38ebc474e1bbb9508fe3f4917cac2
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-26 13:57:28 +0200

    Solving reverse

---
+++ Day-18/reverse.js
@@ -0,0 +1,10 @@
+function main(input) {
+  let array = input.split("\n");
+  let reversed = array[1].split("").reverse().join("").split(".");
+  for (let i = 1; i < array.length; i++) {}
+  return reversed.reverse().join(".");
+}
+console.log(
+  main("2" + "\n" + "i.like.this.program.very.much" + "\n" + "pqr.mno")
+);
+("i.ekil.siht.margorp.yrev.hcum");
