67647c664376c9058e08a0343c37bf7f6470aa33
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-26 14:01:30 +0200

    Reverse

--- Day-18/reverse.js
+++ Day-18/reverse.js
@@ -1,8 +1,13 @@
 function main(input) {
   let array = input.split("\n");
   let reversed = array[1].split("").reverse().join("").split(".");
-  for (let i = 1; i < array.length; i++) {}
-  return reversed.reverse().join(".");
+  let words = [];
+  for (let i = 1; i < array.length; i++) {
+    words.push(
+      array[i].split("").reverse().join("").split(".").reverse().join(".")
+    );
+  }
+  return words.join("\n");
 }
 console.log(
   main("2" + "\n" + "i.like.this.program.very.much" + "\n" + "pqr.mno")
