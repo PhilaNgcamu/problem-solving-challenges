276e3bb8cc2d5376b2675377c435d4afc3730621
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-28 12:04:14 +0200

    Higher Order Functions

--- Day-20/higherOrderFunctions.js
+++ Day-20/higherOrderFunctions.js
@@ -1,22 +1,22 @@
 // -- Reduce()
-const customers = [
-  ["Mark Johansson", "waffle iron", 80, 2],
-  ["Mark Johansson", "blender", 200, 1],
-  ["Mark Johansson", "knife", 10, 4],
-  ["John Doe", "waffle iron", 80, 1],
-  ["John Doe", "knife", 10, 2],
-  ["John Doe", "pot", 20, 3],
-];
-// The task is to convert the customers array to an object using reduce. Each array contains item, price and quantity where, customer[i], i = 0 (name), i = 1 (item), i = 2 (price), i = 3 (quantity)
-const obj = customers.reduce((customerObj, currentValue) => {
-  if (!customerObj[currentValue[0]]) customerObj[currentValue[0]] = [];
-  customerObj[currentValue[0]].push({
-    item: currentValue[1],
-    price: currentValue[2],
-    quantity: currentValue[3],
-  });
-  return { ...customerObj };
-}, {});
+// const customers = [
+//   ["Mark Johansson", "waffle iron", 80, 2],
+//   ["Mark Johansson", "blender", 200, 1],
+//   ["Mark Johansson", "knife", 10, 4],
+//   ["John Doe", "waffle iron", 80, 1],
+//   ["John Doe", "knife", 10, 2],
+//   ["John Doe", "pot", 20, 3],
+// ];
+// // The task is to convert the customers array to an object using reduce. Each array contains item, price and quantity where, customer[i], i = 0 (name), i = 1 (item), i = 2 (price), i = 3 (quantity)
+// const obj = customers.reduce((customerObj, currentValue) => {
+//   if (!customerObj[currentValue[0]]) customerObj[currentValue[0]] = [];
+//   customerObj[currentValue[0]].push({
+//     item: currentValue[1],
+//     price: currentValue[2],
+//     quantity: currentValue[3],
+//   });
+//   return { ...customerObj };
+// }, {});
 // console.log(obj);
 // {
 //   "Mark ...": [{"item": .."price":.., "quantity":... }, {"item": .."price":.., "quantity":... }... ],
@@ -41,4 +41,5 @@ const obj = customers.reduce((customerObj, currentValue) => {
 
 // console.log(flattened);
 
-//get the reduced(sum) value to the 3 x's in the array
+// Count How many instances of values(names) are there by using reduce:
+const names = ["Alice", "Bob", "Tiff", "Bruce", "Alice"];
