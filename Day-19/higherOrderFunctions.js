a5f1b97edcf0eb444b84125263f6f241971c282f
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-28 11:12:39 +0200

    Higher Order Functions

--- Day-19/higherOrderFunctions.js
+++ Day-19/higherOrderFunctions.js
@@ -59,3 +59,17 @@ const customers = [
   ["Mark Johansson", "pot", 20, 3],
 ];
 //The task is to convert the customers array to an object using reduce. Each array contains item, price and quantity where, customer[i], i = 0 (name), i = 1 (item), i = 2 (price), i = 3 (quantity)
+const obj = customers.reduce((customerObj, currentValue) => {
+  customerObj[currentValue[0]] = customerObj[currentValue[0]] || [];
+  customerObj[currentValue[0]].push({
+    item: currentValue[1],
+    price: currentValue[2],
+    quantity: currentValue[3],
+  });
+  return customerObj;
+}, {});
+console.log(obj);
+// {
+//   "Mark ...": [{"item": .."price":.., "quantity":... }, {"item": .."price":.., "quantity":... }... ],
+//   "John Doe": [{"item": .."price":.., "quantity":... }]
+// }
