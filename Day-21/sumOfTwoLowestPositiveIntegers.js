41bfa45acc98806f641df0e6229a48551ab2ccda
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-02 14:33:20 +0200

    Sum of two lowest positive integers

--- Day-21/sumOfTwoLowestPositiveIntegers.js
+++ Day-21/sumOfTwoLowestPositiveIntegers.js
@@ -1,9 +1,9 @@
 // Codewars > Sum of two lowest positive integers
 function sumTwoSmallestNumbers(numbers) {
   //sort the numbers array in decending order
-  const array = numbers.sort((a, b) => a - b);
+  const [num1, num2] = numbers.sort((a, b) => a - b);
   //return the sum of the first two numbers of the sorted array
   console.log(numbers);
-  return array[0] + array[1];
+  return num1 + num2;
 }
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
