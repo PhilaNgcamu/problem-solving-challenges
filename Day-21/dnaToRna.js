a5e93ad65ed77ff7c883acfc6234b4ca3c36503a
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-02 14:23:44 +0200

    DNA to RNA Conversion

--- Day-21/dnaToRna.js
+++ Day-21/dnaToRna.js
@@ -1,4 +1,13 @@
 // Codewars > DNA to RNA Conversion
 function DNAtoRNA(dna) {
-  // create a function which returns an RNA sequence from the given DNA sequence
+  //split the dna
+  const array = dna.split("");
+  //map over the dna and each element should have a complementary then join the elements and return them
+  return array.reduce((result, letter) => {
+    if (letter === "T") result += "U";
+    else result += letter;
+    return result;
+  }, "");
 }
+
+console.log(DNAtoRNA("GCAT"));
