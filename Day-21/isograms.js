b2e514fa94a75cd4474bbdb1b8386090092103a3
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-02 12:44:35 +0200

    Isograms

---
+++ Day-21/isograms.js
@@ -0,0 +1,9 @@
+// Codewars > Isograms
+function isIsogram(str) {
+  //find the length of str
+  const length = str.toLowerCase().length;
+  //find the length of the set and remove duplicate letters if anny
+  const result = new Set(str.toLowerCase());
+  //return if the set === str length then true else false
+  return [...result].length === length;
+}
