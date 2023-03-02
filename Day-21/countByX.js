04164615e1d77182d2994aefc31a60b846e256d3
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-02 14:05:02 +0200

    Count By X

---
+++ Day-21/countByX.js
@@ -0,0 +1,13 @@
+function countBy(x, n) {
+  //store the multiples of x until the numbers == n
+  let z = [];
+  let multiple = 0;
+  //use a for loop of i and push the muliples of x
+  for (let i = 1; i <= n; i++) {
+    multiple += x;
+    z.push(multiple);
+  }
+  //return z
+  return z;
+}
+console.log(countBy(1, 10));
