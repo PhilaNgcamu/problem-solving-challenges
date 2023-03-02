1466c0c36eb257e47c547d5cdc7ee5cbba3978c5
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-02 14:50:51 +0200

    Will You Make it?

--- Day-21/willYouMakeIt.js
+++ Day-21/willYouMakeIt.js
@@ -1,5 +1,7 @@
 //Codewars > Will You Make It?
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   //return true if distanceToPump <= mpg * fuelLeft
-  return distanceToPump <= mpg * fuelLeft ? true : false;
+  return distanceToPump <= mpg * fuelLeft;
 };
+
+console.log(zeroFuel(100, 50, 1));
