b9e2e5de4c17d9b0124060fd02dbc8813bafaecf
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-04 10:48:02 +0200

    To Jaden Cased

---
+++ Day-23/toJadenCase.js
@@ -0,0 +1,9 @@
+// Codewars > Jaden Case
+String.prototype.toJadenCase = function () {
+  let result = "";
+  //split into array
+  let arr = this.split(" ");
+  //map over the array and capitalize the first letter of each word
+  return arr.map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+};
+const str = "How can mirrors be real if our eyes aren't real";
