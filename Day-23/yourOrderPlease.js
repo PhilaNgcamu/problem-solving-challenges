e452425f0bef4a8c4d88838cc19d2e6bac69b76e
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-04 12:14:01 +0200

    Your Order Please!

--- Day-23/yourOrderPlease.js
+++ Day-23/yourOrderPlease.js
@@ -2,6 +2,7 @@
 function order(words) {
   if (!words.length) return words;
   //sort by the ascending order of the numbers
-  console.log("is2".sort());
+  const b = { p: ["p", 2] };
+  console.log(b["p"].find((num) => typeof num === "number"));
 }
 console.log(order("is2 Thi1s T4est 3a"));
