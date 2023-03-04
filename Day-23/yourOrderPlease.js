751532695f8aa26fcf6a175cd8e32c16e3ed608e
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-04 12:06:29 +0200

    Your Order Please!

---
+++ Day-23/yourOrderPlease.js
@@ -0,0 +1,7 @@
+// Codewars > Your order Please!
+function order(words) {
+  if (!words.length) return words;
+  //sort by the ascending order of the numbers
+  console.log("is2".sort());
+}
+console.log(order("is2 Thi1s T4est 3a"));
