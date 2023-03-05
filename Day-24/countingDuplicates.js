7d459da27c5dbf68769330a7438b4058114a0530
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-05 22:12:19 +0200

    Counting Duplicates

--- Day-24/countingDuplicates.js
+++ Day-24/countingDuplicates.js
@@ -1,6 +1,19 @@
 // Codewars > Counting Duplicates
 function duplicateCount(text) {
-  //remove duplicates
-  const set = new Set();
-  //return the length of the removed duplicates - text length
+  //declare array = []
+  const array = [];
+  //split the text
+  const word = text.toLowerCase().split("");
+  //use 2 for loop to check the repeating letter if exists push it to the array and remove it
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < word.length; j++) {
+      if (i === j) continue;
+      else if (word[i] === word[j]) {
+        array.push(word[j]);
+        word.splice(j, 1);
+      }
+    }
+  }
+  return new Set(array).size;
 }
+console.log(duplicateCount("Indivisibilities"));
