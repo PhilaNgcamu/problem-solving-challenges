e2edaa9039403a20e1c2196825b8818ee1dc15be
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-05 18:22:43 +0200

    Find the unique number

--- Day-24/findTheUniqueNumber.js
+++ Day-24/findTheUniqueNumber.js
@@ -1,16 +1,5 @@
 // Codewars > Find the unique number
 function findUniq(arr) {
-  //create an object to store the number of occurances
-  const obj = {};
-  //loop through the array
-  for (let i = 0; i < arr.length; i++) {
-    //if the number exists in an object increment by 1 else assign it to 1
-    if (arr[i] in obj) obj[arr[i]]++;
-    else obj[arr[i]] = 1;
-  }
-  //loop through the object
-  console.log(obj);
-  for (const num in obj) {
-    if (obj[num] === 1) return Number(num);
-  }
+  let result = arr.sort((a, b) => a - b);
+  return result[0] === result[1] ? result.pop() : result[0];
 }
