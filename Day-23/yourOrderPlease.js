9568d721f1bedc4899faa46d9bb882fc5724b23c
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-05 17:19:12 +0200

    Solving Your Order, Please

--- Day-23/yourOrderPlease.js
+++ Day-23/yourOrderPlease.js
@@ -1,8 +1,12 @@
 // Codewars > Your order Please!
 function order(words) {
   if (!words.length) return words;
-  //sort by the ascending order of the numbers
-  const b = { p: ["p", 2] };
-  console.log(b["p"].find((num) => typeof num === "number"));
+  //if the word has number corresponding to an index place the word with the number at that index in an array
+  const list = words.split(" ");
+  const array = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list.includes(i)) array.push(list[i]);
+  }
+  return array;
 }
 console.log(order("is2 Thi1s T4est 3a"));
