5c229be242938412f67c56284ad2a4ca0c3e9e35
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-05 21:32:53 +0200

    Solve Detect Pangram

--- Day-24/detectPangram.js
+++ Day-24/detectPangram.js
@@ -1,10 +1,9 @@
 // Codewars > Detect Pangram
 function isPangram(string) {
-  //Find the length of the string that has been splited withou any whitespace
-  const array = string.split("").filter((letter) => letter !== " ");
-  //find the length of the removed duplicates if there are there
-  const set = new Set(...array);
-  //return true if size of the duplicates === length of the array
-  return set;
+  const array = string
+    .split("")
+    .filter((letter) => /[a-z]/gi.test(letter))
+    .join("");
+  return array.match(/[a-z]/gi).length === array.length;
 }
-console.log(isPangram("The quick brown fox jumps over the lazy dog."));
+console.log(isPangram("This is not a pangram."));
