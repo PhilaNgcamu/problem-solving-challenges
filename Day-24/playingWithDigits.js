77f070e05e3bb7af0c6410f56f5b8fdc973ae8be
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-05 22:34:02 +0200

    Solving Playing With Digits

--- Day-24/playingWithDigits.js
+++ Day-24/playingWithDigits.js
@@ -3,15 +3,13 @@ function digPow(n, p) {
   //split the number into single digits
   const array = n.toString().split("");
   //reduce each number to the power p
-  const numbers = array.reduce(
-    arr,
-    num,
-    (index) => {
-      arr.push(parseInt(num ** p + index));
-      return arr;
-    },
-    []
-  );
+  const numbers = array.reduce((arr, num, index) => {
+    arr.push(parseInt(num ** (p + index)));
+    return arr;
+  }, []);
   //add the numbers
+  const sum = numbers.reduce((sum, num) => sum + num, 0);
   //return the added numbers divided by n
+  return n / sum;
 }
+console.log(digPow(89, 1));
