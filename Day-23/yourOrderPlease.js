27e2465d525c6a5c84be497e3983fa4073598089
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-05 17:40:22 +0200

    Your order, please

--- Day-23/yourOrderPlease.js
+++ Day-23/yourOrderPlease.js
@@ -1,12 +1,12 @@
 // Codewars > Your order Please!
 function order(words) {
   if (!words.length) return words;
-  //if the word has number corresponding to an index place the word with the number at that index in an array
+  //Sort the words by a digit and return them
   const list = words.split(" ");
-  const array = [];
-  for (let i = 1; i <= list.length; i++) {
-    array[i] = list.find((word) => word.includes(i));
-  }
-  return array.filter(Boolean).join(" ");
+  return list
+    .sort((currentNumber, nextNumber) => {
+      return currentNumber.match(/\d+/) - nextNumber.match(/\d+/);
+    })
+    .join(" ");
 }
 console.log(order("is2 Thi1s T4est 3a"));
