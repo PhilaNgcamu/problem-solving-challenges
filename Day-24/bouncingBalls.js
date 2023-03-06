1b81410eb290a244217a7c3c1fef7d45a6052425
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-06 16:01:57 +0200

    Bouncing Balls

--- Day-24/bouncingBalls.js
+++ Day-24/bouncingBalls.js
@@ -1,4 +1,11 @@
 function bouncingBall(h, bounce, window) {
-  // your code here
+  //'h = 30.0, bounce = 0.66, window = 1.5' result = 15
+  //if(h > 0 && bounce >= 0 && bounce < 1 && window < h) return num > 0
+  // else return -1
+  //use a while loop , h > window increment the count by 1
+  var rebounds = -1;
+  if (bounce > 0 && bounce < 1)
+    while (h > window) (rebounds += 2), (h *= bounce);
+  return rebounds;
 }
-console.log(bouncingBall(3.0, 0.66, 1.5));
+console.log(bouncingBall(30, 0.66, 1.5));
