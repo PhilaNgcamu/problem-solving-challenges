8b4cadcb1036f98e0d00bb21740f1937b025af4d
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-07 11:21:55 +0200

    Message Program

---
+++ Day-25-Recursion/messageProgram.js
@@ -0,0 +1,36 @@
+//Lets print "Hello Word" 6 times.
+function main() {
+  message();
+}
+
+function message() {
+  console.log("Hello World");
+  message1();
+}
+
+function message1() {
+  console.log("Hello World");
+  message2();
+}
+
+function message2() {
+  console.log("Hello World");
+  message3();
+}
+
+function message3() {
+  console.log("Hello World");
+  message4();
+}
+
+function message4() {
+  console.log("Hello World");
+  message5();
+}
+
+function message5() {
+  console.log("Hello World");
+  message1();
+}
+
+main();
