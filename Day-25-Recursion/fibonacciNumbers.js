a0ad9e18d8ae7dcaf523242b6dc9ff06eff13311
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-07 13:20:55 +0200

    Fibonacci Numbers

--- Day-25-Recursion/fibonacciNumbers.js
+++ Day-25-Recursion/fibonacciNumbers.js
@@ -1,2 +1,7 @@
 // Find the nth fibonacci number
 // Where: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34....
+// Steps:
+// Find the 3rd Fibonacci number:
+// f2 = f2-2 + f2-1 = 0 + 1 = 1
+//f3 = f3-2 + f3-1 = 1 + 1 = 2
+//Therefore, fn = fn-2 + fn-1
