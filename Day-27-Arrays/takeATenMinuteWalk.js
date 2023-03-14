dd84c4a6f1aeec39b1101a97c36e157dbfa19be9
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-14 13:49:20 +0200

    Take a Ten Minute Walk

---
+++ Day-27-Arrays/takeATenMinuteWalk.js
@@ -0,0 +1,20 @@
+// Codewars > Take a Ten Minutes Walk
+function isValidWalk(walk) {
+  // declare the obj
+  const obj = {};
+  //loop over each letter
+  for (let i = 0; i < walk.length; i++) {
+    // if the letter exists in the object increment, else assign 1
+    obj[walk[i]] = (obj[walk[i]] || 0) + 1;
+  }
+  // let the sum = first number of the array
+  const array = [...Object.values(obj)];
+  let sum = array[0];
+  // loop over the array, if sum > 0 then sum -= letter occurance else sum += letter occurance
+  for (let i = 1; i < array.length; i++) {
+    if (sum > 0) sum -= array[i];
+    else if (sum < 0) sum += array[i];
+  }
+  //return sum === 0
+  return sum;
+}
