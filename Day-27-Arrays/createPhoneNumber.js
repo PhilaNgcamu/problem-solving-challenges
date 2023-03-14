d130ab454ca1e05be7b2fc6ae6c0b8511449d082
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-14 12:16:47 +0200

    Create Phone Numbers

--- Day-27-Arrays/createPhoneNumber.js
+++ Day-27-Arrays/createPhoneNumber.js
@@ -1,2 +1,7 @@
 // Codewars > Create Phone Numbers
-function createPhoneNumber(numbers) {}
+function createPhoneNumber(numbers) {
+  let str = numbers.join("");
+  //if the string length = 3 then enclone with brackets and then add space
+  let s = numbers.join("").replace(/\d{3,3}/, `(${str.slice(0, 3)}) `);
+  return s.replace(/\d{4}$/, `-${s.slice(-4)}`);
+}
