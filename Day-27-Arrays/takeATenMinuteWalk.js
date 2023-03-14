efb04bcc38ddea27ae175c3de60ce412d63ff3e6
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-14 16:20:00 +0200

    Take A Ten Minute Walk

--- Day-27-Arrays/takeATenMinuteWalk.js
+++ Day-27-Arrays/takeATenMinuteWalk.js
@@ -1,6 +1,5 @@
-// Codewars > Take a Ten Minutes Walk
 function isValidWalk(walk) {
-  if (walk.length === 1 || walk.length !== 10 || !walk.length) return false;
+  if (walk.length !== 10) return false;
   // declare the obj
   const obj = {};
   //loop over each letter
@@ -8,16 +7,7 @@ function isValidWalk(walk) {
     // if the letter exists in the object increment, else assign 1
     obj[walk[i]] = (obj[walk[i]] || 0) + 1;
   }
-  // let the sum = first number of the array
-  const array = [...Object.values(obj)];
-  let sum = array[0];
-  // loop over the array, if sum > 0 then sum -= letter occurance else sum += letter occurance
-  for (let i = 1; i < array.length; i++) {
-    if (sum > 0) sum -= array[i];
-    else if (sum < 0) sum += array[i];
-    else if (sum === 0) return true;
-  }
-  console.log(sum);
-  //return sum === 0
-  return sum === 0;
+  //if array length is odd then return false
+  //return object of n === object of s and object of e === object of w
+  return obj["n"] === obj["s"] && obj["e"] === obj["w"];
 }
