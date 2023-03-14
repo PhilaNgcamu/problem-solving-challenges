405fdba99917a13449dd7794b321e11f7a15b575
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-14 13:06:29 +0200

    Duplicate Encoder

--- Day-27-Arrays/duplicateEncoder.js
+++ Day-27-Arrays/duplicateEncoder.js
@@ -1,22 +1,12 @@
 function duplicateEncode(word) {
-  let str = word.toLowerCase();
-  // declare a new string for new characters
-  let string = "";
-  // create an object to store the numbers of ocurrances
-  let obj = {};
-  // loop over the string.
-  for (let i = 0; i < str.length; i++) {
-    obj[str[i]] = (obj[str[i]] || 0) + 1;
-  }
-  // loop over the string.
-  for (let i = 0; i < str.length; i++) {
-    // if the letter === 1 then add "'('" else "')'" in the string variable
-    if (obj[str[i]] === 1) {
-      string += "(";
-    } else if (obj[str[i]] > 1) string += ")";
-  }
-  // return the string
-  return string;
+  // All the words should be in lower case, then split the word string
+  let array = word.toLowerCase().split("");
+  // map over each letter and ... if the index of the letter === last index of the letter then return "(" else ")"
+  let char = array.map((letter, _, word) =>
+    word.indexOf(letter) === word.lastIndexOf(letter) ? "(" : ")"
+  );
+  //return the joined char
+  return char.join("");
 }
 
 console.log(duplicateEncode("recede"));
