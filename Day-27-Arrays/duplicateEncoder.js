85543f7c2647930f5cce62f7c0ede2178fb8af2e
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-14 12:50:16 +0200

    Solve Duplicate Encoder

--- Day-27-Arrays/duplicateEncoder.js
+++ Day-27-Arrays/duplicateEncoder.js
@@ -1,13 +1,17 @@
 function duplicateEncode(word) {
+  let str = word.toLowerCase();
   // declare a new string for new characters
   let string = "";
   // create an object to store the numbers of ocurrances
   let obj = {};
   // loop over the string.
-  for (let i = 0; i < word.length; i++) {
-    obj[word[i]] = (obj[word[i]] || 0) + 1;
+  for (let i = 0; i < str.length; i++) {
+    obj[str[i]] = (obj[str[i]] || 0) + 1;
   }
   return obj;
+  // loop over the object.
   // if the letter === 1 then add "'('" else "')'" in the string variable
   // return the string
 }
+
+console.log(duplicateEncode("recede"));
