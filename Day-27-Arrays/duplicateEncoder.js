d3215d43e0dbcbf3a831c15b00fc2d654f63826e
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-14 12:47:30 +0200

    Duplicate Encoder

--- Day-27-Arrays/duplicateEncoder.js
+++ Day-27-Arrays/duplicateEncoder.js
@@ -2,7 +2,12 @@ function duplicateEncode(word) {
   // declare a new string for new characters
   let string = "";
   // create an object to store the numbers of ocurrances
+  let obj = {};
   // loop over the string.
+  for (let i = 0; i < word.length; i++) {
+    obj[word[i]] = (obj[word[i]] || 0) + 1;
+  }
+  return obj;
   // if the letter === 1 then add "'('" else "')'" in the string variable
   // return the string
 }
