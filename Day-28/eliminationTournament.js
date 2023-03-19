be48a12e58f5a6423c646ae86c9411c6c6fdc03f
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-19 08:05:09 +0200

    Solving elimination tornament

--- Day-28/eliminationTournament.js
+++ Day-28/eliminationTournament.js
@@ -4,19 +4,21 @@ const tourney = (array) => {
   const output = Array(Math.floor(array.length / 2)).fill([]);
   output[0] = array;
   //use a for loop to traverse the array
-  while (output.includes([].length)) {
-    for (let i = 0; i < array.length; i += 2) {
+  let index = 0;
+  while (index < output.length) {
+    for (let i = 0; i < output[index].length; i += 2) {
       //if array.length is odd then the last element should be first i  the second array
-      if (array.length % 2 !== 0 && i === 0) {
-        output[1] = array.at(-1);
+      if (array.length % 2 !== 0 && !output[1].length && i === 0) {
+        output[1].push(array.at(-1));
       }
       //if the arr[i] > arr[i + 1] push arr[i] to output else push arr[i + 1]
-      else if (array[i] > array[i + 1]) output[1].push(array[i]);
-      else output[1].push(array[i + 1]);
+      //         else if (array[i] > array[i + 1]) output[i + 1].push(array[i]);
+      //         else output[i + 1].push(array[i + 1]);
     }
+    index++;
   }
 
-  console.log(output.includes([]));
+  console.log(output);
 
   return output;
 };
