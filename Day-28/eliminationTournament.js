f2faaf8f2609aae31345d089f3f78ca6e1f6969e
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-20 07:53:35 +0200

    Solving Elimination Tournament

--- Day-28/eliminationTournament.js
+++ Day-28/eliminationTournament.js
@@ -1,12 +1,12 @@
 // Codewars > Elimination Tourney
 const tourney = (array) => {
   //declare the output array to push arrays, where output at 0 == array
-  const output = Math.ceil(Array(array.length / 2)).fill([]);
+  const output = Array(Math.ceil(array.length / 2)).fill([]);
   output[0] = [...array];
   //while on the first index of the output, loop through the first array
 
   //--if arr[i] > arr[i + 1] then push arr[i] output[index + 1] else push arr[i + 1]
-  //return output
+  return output;
 };
 
 console.log(tourney([9, 5, 4, 7, 6, 3, 8]));
