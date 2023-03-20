ce204f029a5b461860132c70456e53f0c5ac663c
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-20 08:13:20 +0200

    Solving Elimination Tournament

--- Day-28/eliminationTournament.js
+++ Day-28/eliminationTournament.js
@@ -4,16 +4,16 @@ const tourney = (array) => {
   const output = Array(Math.ceil(array.length / 2)).fill([]);
   output[0] = [...array];
   //while on the first index of the output, loop through the existing array
-  output.forEach((arr, index) => {
-    for (let i = 0; i < arr.length; i += 2) {
-      //--if arr.length is odd then push last character to the next arr
-      if (arr.length % 2 !== 0 && i === 0 && !output[index + 1].length) {
-        output[index + 1].push(arr.at(-1));
-      }
-
+  for (let i = 0; i < output.length; i++) {
+    if (output.at(-1).length) break;
+    //--if arr.length is odd then push last character to the next arr
+    if (output[0].length % 2 !== 0) output[1].push(output[0].at(-1));
+    for (let j = 0; j < output[i].length; j += 2) {
       //--if arr[i] > arr[i + 1] then push arr[i] output[index + 1] else push arr[i + 1]
+      if (output[i][j] >= output[i][j + 1]) output[i + 1].push(output[i][j]);
+      else output[i + 1].push(output[i][j + 1]);
     }
-  });
+  }
 
   return output;
 };
