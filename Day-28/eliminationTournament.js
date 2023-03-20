03952078fb8ee32f8708fe0d74a845f6fe484ac5
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-20 07:43:55 +0200

    Solving Elimination Tournament

--- Day-28/eliminationTournament.js
+++ Day-28/eliminationTournament.js
@@ -1,25 +1,26 @@
 // Codewars > Elimination Tourney
 const tourney = (array) => {
   //declare the output array to push arrays, where output at 0 == array
-  const output = Array(Math.floor(array.length / 2)).fill([]);
+  const output = Array(Math.ceil(array.length / 2)).fill([]);
   output[0] = array;
   //use a for loop to traverse the array
-  let index = 0;
-  while (index < output.length) {
+  let index = output.length - 1;
+  while (index >= 0) {
     for (let i = 0; i < output[index].length; i += 2) {
       //if array.length is odd then the last element should be first i  the second array
-      if (array.length % 2 !== 0 && !output[1].length) {
+      if (output[index].length % 2 !== 0 && index === 0) {
         output[1].push(array.at(-1));
         break;
+      } else {
+        output[index + 1].push(output[index][i]);
       }
-      //if the arr[i] > arr[i + 1] push arr[i] to output else push arr[i + 1]
-      //       else if (output[i] > array[i + 1]) output[i + 1].push(array[i]);
-      //       else output[i + 1].push(array[i + 1]);
     }
-    index++;
+    index--;
   }
 
-  console.log(output);
+  // console.log(output);
 
   return output;
 };
+
+console.log(tourney([9, 5, 4, 7, 6, 3, 8]));
