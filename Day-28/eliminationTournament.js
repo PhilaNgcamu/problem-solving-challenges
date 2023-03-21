e9a51d7f799f7617ebd72977916098b65a918174
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-21 09:02:52 +0200

    Elimination Tornament

--- Day-28/eliminationTournament.js
+++ Day-28/eliminationTournament.js
@@ -1,24 +1,13 @@
 // Codewars > Elimination Tourney
-const tourney = (array) => {
-  //declare the output array to push arrays, where output at 0 == array
-  const output = Array(Math.ceil(array.length / 2)).fill([]);
-  output[0] = [...array];
-  //while on the first index of the output, loop through the existing array
-  for (let i = 0; i < output.length; i++) {
-    if (output.at(-1).length) break;
-    //--if arr.length is odd then push last character to the next arr
-    else if (output[0].length % 2 !== 0) output[1].push(output[0].at(-1));
-    for (let j = 0; j < output[i].length; j += 2) {
-      //--if arr[i] > arr[i + 1] then push arr[i] output[index + 1] else push arr[i + 1]
-      if (!output[i][j + 1]) break;
-      else if (output[i][j] >= output[i][j + 1]) {
-        output[i + 1].push(output[i][j]);
-      } else {
-        output[i + 1].push(output[i][j + 1]);
-      }
-    }
-  }
-  return output;
+const tourney = (arr) => {
+  if (arr.length === 1) return [arr];
+
+  let next = arr.length % 2 ? arr.slice(-1) : [];
+
+  for (let i = 0; i < arr.length - 1; i += 2)
+    next.push(Math.max(arr[i], arr[i + 1]));
+
+  return [arr].concat(tourney(next));
 };
 
 console.log(tourney([9, 5, 4, 7, 6, 3, 8]));
