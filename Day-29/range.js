bfd2f5f1f77ee64a4be7de057d70ff2729f86e9e
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-21 20:26:21 +0200

    Range

--- Day-29/range.js
+++ Day-29/range.js
@@ -1,6 +1,6 @@
 // Write a JavaScript program to get the integers in range (x, y)
 function range(x, y) {
-  if (x === y) return y;
-  return [x, range(x + 1, y)].flat();
+  if (x === y) return y - 1;
+  return [x + 1, range(x + 1, y)].flat();
 }
 console.log(range(2, 9));
