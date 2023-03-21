a69810637dc078289bcd58830c7e046a044ea566
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-21 10:00:03 +0200

    A Needle in the Haystack

---
+++ Day-29/needleInAHaystack.js
@@ -0,0 +1,6 @@
+// Codewars > A Needle in the Haystack
+function findNeedle(haystack) {
+  return haystack.indexOf("needle")
+    ? "found the needle at position " + haystack.indexOf("needle")
+    : -1;
+}
