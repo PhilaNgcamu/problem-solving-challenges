9a4235ea2dd5a1c10a4f56502a3d2d41395e1bd1
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-21 15:12:34 +0200

    Count Positives Sum Negatives

--- Day-29/countPositivesSumNegatives.js
+++ Day-29/countPositivesSumNegatives.js
@@ -1,8 +1,9 @@
 // Codewars > Count of positives / sum of negatives
 function countPositivesSumNegatives(input) {
-  const count = input.filter((num) => num > 0).length;
-  const sum = input.filter((num) => num < 0).reduce((a, b) => a + b, 0);
-  if ((count && !sum) || (!count && sum)) return [count, sum];
-  else if (!count && !sum) return [];
-  return [count, sum];
+  return input && input.length
+    ? [
+        input.filter((p) => p > 0).length,
+        input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
+      ]
+    : [];
 }
