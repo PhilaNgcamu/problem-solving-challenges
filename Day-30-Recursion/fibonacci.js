cd5b61f788d4671a848dcb3a29346852597732a7
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-22 14:56:06 +0200

    Fibonacci

--- Day-30-Recursion/fibonacci.js
+++ Day-30-Recursion/fibonacci.js
@@ -1,6 +1,8 @@
 function fibonacci(num) {
-  if (num <= 1) return 1;
-  return fibonacci(num - 1) + fibonacci(num - 2);
+  if (num <= 1) return [0, 1];
+  const numbers = fibonacci(num - 1);
+  numbers.push(numbers[numbers.length - 1] + numbers[numbers.length - 2]);
+  return numbers;
 }
 
 console.log(fibonacci(3));
