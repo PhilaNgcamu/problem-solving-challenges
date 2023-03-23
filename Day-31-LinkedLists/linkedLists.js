e50338eab0635a24ef322885f687fd724e15bd26
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-23 17:33:22 +0200

    Linked Lists

--- Day-31-LinkedLists/linkedLists.js
+++ Day-31-LinkedLists/linkedLists.js
@@ -1,11 +1,6 @@
-const node1 = {
-  data: 100,
-};
-
-const node2 = {
-  data: 200,
-};
-
-node1.next = node2;
-
-console.log(node1);
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
