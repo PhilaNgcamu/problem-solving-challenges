9e738e64b8df0590e1c49b213314740f557f6c2d
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-27 18:31:43 +0200

    Linked List

--- Day-32-LinkedList/linkedList.js
+++ Day-32-LinkedList/linkedList.js
@@ -19,20 +19,42 @@ class LinkedList {
   insertTheLastNode(data) {
     if (!this.head) {
       this.head = new Node(data, this.head);
-      this.size++;
     } else {
       let currentNode = this.head;
       while (currentNode.pointer) {
         currentNode = currentNode.pointer;
       }
       currentNode.pointer = new Node(data);
-      this.size++;
     }
+    this.size++;
+  }
+  // Insert data at an index
+  insertAtIndex(data, index) {
+    this.size++;
+    if (!this.head) {
+      this.head = new Node(data);
+    } else if (index >= this.size) return null;
+    else if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    let count = 0;
+    let currentNode = this.head;
+    let previousNode;
+    const node = new Node(data, this.head);
+    while (count < index) {
+      previousNode = currentNode;
+      currentNode = currentNode.pointer;
+      count++;
+    }
+    node.pointer = currentNode;
+    previousNode.pointer = node;
   }
 }
 
 const linkedList = new LinkedList();
 linkedList.insertFirstNode(100);
 linkedList.insertTheLastNode(200);
+linkedList.insertAtIndex(4, 1);
 
 console.log(linkedList);
