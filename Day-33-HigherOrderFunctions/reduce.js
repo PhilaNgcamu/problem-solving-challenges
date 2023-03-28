60e1ced6d1a6b612a5f8b68a02c09a504756d816
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-28 15:37:05 +0200

    Reduce Method

--- Day-33-HigherOrderFunctions/reduce.js
+++ Day-33-HigherOrderFunctions/reduce.js
@@ -10,3 +10,29 @@ function number(accumulator, value, index) {
 
 const reduce = array1.reduce(number, 0);
 console.log(reduce);
+
+const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
+const sum = objects.reduce(
+  (accumulator, currentValue) => accumulator + currentValue.x,
+  0
+);
+
+const friends = [
+  {
+    name: "Anna",
+    books: ["The War Story", "Harry Potter"],
+    age: 21,
+  },
+  {
+    name: "Bob",
+    books: ["War and peace", "Romeo and Juliet"],
+    age: 26,
+  },
+  {
+    name: "Alice",
+    books: ["The Lord of the Rings", "The Shining"],
+    age: 18,
+  },
+];
+
+console.log(sum);
