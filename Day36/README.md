9d37c1a9c5249e3765353e54c92967e4deec605d
PhilaNgcamu <philangcamu17@gmail.com>
2023-04-19 09:18:02 +0200

    Length Property on Strings and Arrays

---
+++ Day36/README.md
@@ -0,0 +1,3 @@
+# Length property on Strings And Arrays
+
+Solved 1 challenge this day.
---
+++ Day36/length.js
@@ -0,0 +1,32 @@
+// const str = "Life, the universe and everything. Answer:";
+// // Expected output: "Life, the universe and everything. Answer: 42"
+
+// function getCharacterLength(str) {
+//   return [...str].length;
+// }
+// console.log(getCharacterLength("A\n\n5"));
+
+// const x = "John";
+// const empty = "";
+
+// console.log(`${x} is ${x.length} code units long`);
+// //Expected output: John is 4 code units long
+
+// function func1() {
+//   console.log(arguments.length);
+// }
+
+// func1("n", "p", "cee");
+
+// function adder(base) {
+//   console.log(arguments);
+//   for (let i = 1; i < arguments.length; i++) {
+//     base += Number(arguments[i]);
+//   }
+//   return base.length;
+// }
+// console.log(adder("33", "66"));
+
+//["Andile, "Kelebogile", "Philasande"]
+//Convert the array into its length
+//filter out for the required length
