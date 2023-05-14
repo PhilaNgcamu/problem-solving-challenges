e6407de60aba8a6a27bf12bde09b7ec188107127
PhilaNgcamu <philangcamu17@gmail.com>
2023-05-14 16:37:50 +0200

    Exploring Degugging Tools

--- Day-37/errorHandling.js
+++ Day-37/errorHandling.js
@@ -70,16 +70,46 @@
 //   })()
 // );
 
-function main() {
-  print(1);
-}
+// function main() {
+//   print(1);
+// }
 
-function print(num) {
-  if (num === 5) console.log(num);
-  else {
-    console.log(num);
-    print(num + 1);
-  }
-}
+// function print(num) {
+//   if (num === 5) console.log(num);
+//   else {
+//     console.log(num);
+//     print(num + 1);
+//   }
+// }
 
-main();
+// main();
+
+// function main() {
+//   print1(1);
+// }
+
+// function print1(num) {
+//   console.log(num);
+//   print2(2);
+// }
+
+// function print2(num) {
+//   console.log(num);
+//   print3(3);
+// }
+
+// function print3(num) {
+//   console.log(num);
+//   print4(4);
+// }
+
+// function print4(num) {
+//   console.log(num);
+//   print5(5);
+// }
+
+// function print5(num) {
+//   console.log(num);
+// }
+
+// main();
