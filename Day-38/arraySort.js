262aa88c096aac18f0e211cacdd1be64932357c5
PhilaNgcamu <philangcamu17@gmail.com>
2023-05-16 13:52:35 +0200

    Array Sort

--- Day-38/arraySort.js
+++ Day-38/arraySort.js
@@ -1,3 +1,24 @@
+function sortByMostLikes(arr) {
+  let mostAppearedAuthor;
+  for (let i = 0; i < arr.length; i++) {
+    let count = 0;
+    if (arr.filter((obj) => obj.author === arr[i].author).length > count) {
+      mostAppearedAuthor = arr[i].author;
+    }
+  }
+
+  console.log(
+    arr
+      .filter((obj) => obj.author === mostAppearedAuthor)
+      .reduce((acc, obj) => {
+        acc["author"] = obj.author;
+        if (acc["likes"]) acc["likes"] += obj.likes;
+        else acc["likes"] = obj.likes;
+
+        return acc;
+      }, {})
+  );
+}
 const arr = [
   { author: "Michael Chan", likes: 7 },
   { author: "Edsger W. Dijkstra", likes: 5 },
@@ -6,20 +27,4 @@ const arr = [
   { author: "Robert C. Martin", likes: 0 },
   { author: "Robert C. Martin", likes: 2 },
 ];
-let mostAppearedAuthor;
-for (let i = 0; i < arr.length; i++) {
-  let count = 0;
-  if (arr.filter((obj) => obj.author === arr[i].author).length > count) {
-    mostAppearedAuthor = arr[i].author;
-  }
-}
-
-console.log(
-  arr
-    .filter((obj) => obj.author === mostAppearedAuthor)
-    .reduce((acc, obj) => {
-      acc += obj.likes;
-      console.log(acc);
-      return { author: mostAppearedAuthor, likes: acc };
-    }, 0)
-);
+sortByMostLikes(arr);
