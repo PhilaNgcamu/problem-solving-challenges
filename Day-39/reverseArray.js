33f1967f90636e5b65038a5d645dbc0cdca318d6
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-19 10:28:06 +0200

    Reverse an array

--- Day-39/reverseArray.js
+++ Day-39/reverseArray.js
@@ -1,19 +1,30 @@
 function reverseArray(arr) {
   const array = [...arr];
   const result = [];
+  let i = array.length - 1;
+  while (i >= 0) {
+    result.push(arr[i]);
+    i--;
+  }
+  return result;
+}
+
+function reverseArray(arr) {
+  const array = [...arr];
+  const result = [];
   for (let i = array.length - 1; i >= 0; i--) {
     result.push(arr[i]);
   }
   return result;
 }
 
-// function reverseArray(arr) {
-//     const array = [...arr];
-//     const result = [];
-//     for (const elem of array) {
-//       result.push(elem);
-//     }
-//     return result;
-//   }
+function reverseArray(arr) {
+  const array = [...arr];
+  const result = [];
+  for (const elem of array) {
+    result.unshift(elem);
+  }
+  return result;
+}
 
-console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(reverseArray([1, 2, 3, 4, 5, 6, 3]));
