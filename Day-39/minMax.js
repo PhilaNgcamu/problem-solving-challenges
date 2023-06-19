7d4d6b2cf5875df13b4315211dab70aa6bfdfcc2
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-19 12:17:30 +0200

    Min Max

--- Day-39/minMax.js
+++ Day-39/minMax.js
@@ -24,4 +24,22 @@ function minMax(arr) {
   return [min, max];
 }
 
+function minMax(arr) {
+  const array = [...arr];
+  const sortedArr = array.sort((a, b) => a - b);
+  return [sortedArr[0], sortedArr.at(-1)];
+}
+
+function minMax(arr) {
+  const array = [...arr];
+  let min = array[0];
+  let max = array[0];
+  let i = 1;
+  for (const num of array) {
+    if (num < min) min = num;
+    if (num > max) max = num;
+  }
+  return [min, max];
+}
+
 console.log(minMax([-4, 4, 2, 5, 33, 5, 9, 19]));
