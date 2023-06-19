bf78224f1609f8786978c80f1be24de0b378fe4c
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-19 12:32:19 +0200

    Remove duplicates

---
+++ Day-39-ArrayManipulation/removeDuplicates.js
@@ -0,0 +1,10 @@
+//Remove Duplicates: Write a function to remove duplicate elements from an array.
+function removeDuplicates(arr) {
+  return arr;
+}
+
+console.log(
+  removeDuplicates([
+    1, 1, 1, 13, 3, 3, 3, 32, 42, 4, 5, 5, 5, 7, 735, 3, 2, 1, 5,
+  ])
+);
