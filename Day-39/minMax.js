a09aac26360d781807b3e860076882d4ffbd8c8a
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-19 10:40:09 +0200

    Find Min Max

--- Day-39/minMax.js
+++ Day-39/minMax.js
@@ -1,1 +1,14 @@
 //Find the Maximum and Minimum: Write a function to find the maximum and minimum values in an array.
+
+function minMax(arr) {
+  const array = [...arr];
+  let min = array[0];
+  let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+  }
+  return [min, max];
+}
+
+console.log(minMax([-4, 4, 2, 5, 33, 5, 9, 19]));
