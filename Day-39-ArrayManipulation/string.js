a29dad6e296dd1661b875273c520ec71ac62a92a
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-21 12:29:21 +0200

    String Manipulation

--- Day-39-ArrayManipulation/string.js
+++ Day-39-ArrayManipulation/string.js
@@ -1,7 +1,17 @@
-let str = "Hello, world! How are you?";
+let str = "xxxxXX";
 
 console.log(
-  str.replace(/[a-z]+/gi, (matched, value1, value2, offset, input) => {
-    return matched.split("").reverse().join("");
+  str.replace(/x/gi, (matched, value1, value2, offset, input) => {
+    if (matched === "x") {
+      return (matched = String.fromCharCode(
+        97 + Math.round(Math.random() * (122 - 97))
+      ));
+    }
+    return (matched = String.fromCharCode(65 + Math.round(Math.random() * 25)));
   })
 );
+// Challenge: Determine if a string is a pangram (contains all the letters of the alphabet).
+// Example:
+// Input: 'The quick brown fox jumps over the lazy dog'
+// Output: true
+// Method(s) to use: Combination of split(), filter(), test() and custom logic(Set is allowed).
