21fd1632a68fada53be551a79dba89ca197cd991
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-26 12:55:56 +0200

    JavaScript Algorithms and Data Structures Masterclass on Udemy

--- Day-40/bigO.js
+++ Day-40/bigO.js
@@ -1,14 +1,18 @@
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 0; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
 function addUpTo(n) {
-  let total = 0;
-  for (let i = 0; i <= n; i++) {
-    total += i;
-  }
-  return total;
+  return (n * (n + 1)) / 2;
 }
-addUpTo(1000000000);
 
 const t1 = performance.now();
 console.log(`Time Elapsed: ${t1 / 1000} seconds`);
+addUpTo(1000000000);
 
 const t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+console.log(`Time Elapsed: ${t2 / 1000} seconds`);
