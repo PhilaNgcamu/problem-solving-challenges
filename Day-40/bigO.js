5f91e3555ab9b0d2761a309f09f4348bccef68a3
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-26 14:23:48 +0200

    Big O Notation

--- Day-40/bigO.js
+++ Day-40/bigO.js
@@ -13,4 +13,21 @@ function addUpTo(n) {
   return (n * (n + 1)) / 2;
 }
 
-addUpTo(1000000000);
+function countUpAndDown(n) {
+  console.log("Going Up!");
+
+  //For the first loop below the number of operations is O(n)
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+
+  console.log("At the top!\nGoing Down...");
+
+  //For the second loop below the number of operations is alse O(n)
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("Back down. Bye");
+}
+
+countUpAndDown(10);
