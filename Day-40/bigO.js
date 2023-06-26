dacda088c2e1edcdb88877f7008ccea7c91c78b6
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-26 14:14:56 +0200

    Understanding Big O Notation

--- Day-40/bigO.js
+++ Day-40/bigO.js
@@ -1,18 +1,16 @@
-// function addUpTo(n) {
-//   let total = 0;
-//   for (let i = 0; i <= n; i++) {
-//     total += i;
-//   }
-//   return total;
-// }
+// The number of operations below is O(n)
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+// The number of operations below is O(1)
 
 function addUpTo(n) {
   return (n * (n + 1)) / 2;
 }
 
-const t1 = performance.now();
-console.log(`Time Elapsed: ${t1 / 1000} seconds`);
 addUpTo(1000000000);
-
-const t2 = performance.now();
-console.log(`Time Elapsed: ${t2 / 1000} seconds`);
