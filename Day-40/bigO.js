63e1a1299b73d0663588e58aa92ee6b1e518683f
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-26 12:49:52 +0200

    Peformance of the code executed

--- Day-40/bigO.js
+++ Day-40/bigO.js
@@ -5,9 +5,10 @@ function addUpTo(n) {
   }
   return total;
 }
+addUpTo(1000000000);
 
 const t1 = performance.now();
-addUpTo(10000000000);
-const t2 = performance.now();
+console.log(`Time Elapsed: ${t1 / 1000} seconds`);
 
+const t2 = performance.now();
 console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
