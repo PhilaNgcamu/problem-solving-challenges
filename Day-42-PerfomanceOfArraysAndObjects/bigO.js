c77176970ae826172864d3aa4bfd035fff4be07f
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-28 02:21:04 +0200

    Analyzing the perfomance of Arrays and Objects

---
+++ Day-42-PerfomanceOfArraysAndObjects/bigO.js
@@ -0,0 +1,11 @@
+// The instructor object based on Big o:
+// - Insertion - O(1)
+// - Removal - O(1)
+// - Searching - O(N)
+// - Access - O(1)
+
+const instructor = {
+  firstName: "Kelly",
+  isInstructor: true,
+  favouriteNumbers: [1, 2, 3, 4],
+};
