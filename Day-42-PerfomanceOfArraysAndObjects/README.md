74c1d53a39f41aaa1964add7afc418afceb53c7d
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-28 14:42:49 +0200

    Introduction to Problem Solving

--- Day-42-PerfomanceOfArraysAndObjects/README.md
+++ Day-42-PerfomanceOfArraysAndObjects/README.md
@@ -33,3 +33,12 @@ Link to [JavaScript Algorithms and Data Structures Masterclass on Udemy](https:/
 - Shift, unshift - O(N)
 - sort - O(N \* log N)
 - forEach/map/filter/reduce/etc. - O(N)
+
+<h1>Problem Solving</h1>
+<h2>Step 1: Understand the problem</h2>
+
+- 1. Can I restate the problem in my own words?
+- 2. What are the inputs that go into the problem?
+- 3. What are the outputs that should come from the solution to the problem? (what should the output look like)
+- 4. Can the outputs be determined from the inputs? (Do I have enough information to solve the problem)
+- 5. How should I label important pieces of data that are part of the problem?
---
+++ Day-42-PerfomanceOfArraysAndObjects/introToProblemSolving.js
@@ -0,0 +1,13 @@
+// Write a function which two numbers and returns their sum
+
+// 1. Can I restate the problem in my own words?
+// add 2 numbers
+// 2. What are the inputs that go into the problem?
+// Is it floats, integers or strings of numbers
+// Assume that it always takes 2 numbers
+// 3. What are the outputs that should come from the solution to the problem? (what should the output look like)
+// should the out be an integer, float or string?
+// 4. Can the outputs be determined from the inputs? (Do I have enough information to solve the problem)
+// If yes, then you can jump to number. If no, take your time to understand the problem
+// 5. How should I label important pieces of data that are part of the problem?
+// Write a function that takes in 2 arguments? then return their sum defined in a variable or not.
