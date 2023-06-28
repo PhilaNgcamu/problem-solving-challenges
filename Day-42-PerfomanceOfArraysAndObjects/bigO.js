6a550e56c4d5a125a313f85a1342d958fdba89d2
PhilaNgcamu <philangcamu17@gmail.com>
2023-06-28 02:40:54 +0200

    Big O of Arrays

--- Day-42-PerfomanceOfArraysAndObjects/bigO.js
+++ Day-42-PerfomanceOfArraysAndObjects/bigO.js
@@ -9,3 +9,10 @@ const instructor = {
   isInstructor: true,
   favouriteNumbers: [1, 2, 3, 4],
 };
+
+// The names array:
+const names = ["Philasande", "Happy", "Ngcamu"];
+// Insertion - It depends, if I want to insert a value at the beginning of the array then the perfomance will be slower than adding it at the end because the array re-initializes indeces to values.
+// Removal - It depends, if I want to remove a value from the array then the perfomance will be slower because the array re-initializes indeces to values.
+// Searching - O(N)
+// Access - O(N)
