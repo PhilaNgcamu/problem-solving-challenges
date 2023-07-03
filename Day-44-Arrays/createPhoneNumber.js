b8e9dd1d8bf17603d7c2136c0672cbd36f8cff6b
PhilaNgcamu <philangcamu17@gmail.com>
2023-07-03 14:21:41 +0200

    Create Phone Number

--- Day-44-Arrays/createPhoneNumber.js
+++ Day-44-Arrays/createPhoneNumber.js
@@ -11,4 +11,13 @@ function createPhoneNumber(arr) {
   return copy.join("");
 }
 
+function createPhoneNumber(arr) {
+  const copy = [...arr];
+  let format = "(xxx) xxx-xxxx";
+  for (let i = 0; i < copy.length; i++) {
+    format = format.replace("x", arr[i]);
+  }
+  return format;
+}
+
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
