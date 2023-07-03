48ed28a92d1bf4271fdd55b06afae382025158df
PhilaNgcamu <philangcamu17@gmail.com>
2023-07-03 15:01:08 +0200

    Array diff

--- Day-44-Arrays/arraydiff.js
+++ Day-44-Arrays/arraydiff.js
@@ -1,7 +1,21 @@
 // Codewars > Array.diff
 
 function arrayDiff(a, b) {
-  return a.includes(b[0]) ? [] : a;
+  //have a copy of a
+  //convert copy of a to a string
+
+  let copyOfA = [...a].join("");
+  //use a for loop on
+  for (let i = 0; i < b.length; i++) {
+    //--> if an element in b exists in stringified copy of a then replace that with an empty string globally
+    if (copyOfA.includes(b[i]))
+      copyOfA = copyOfA.replace(new RegExp(b[i], "g"), "");
+  }
+
+  // convert copy of a into an array
+
+  //return the copy
+  return copyOfA;
 }
 
-console.log(arrayDiff([1], [3])); //--> []
+console.log(arrayDiff([3, 3, -5], [3])); //--> [1,2]
