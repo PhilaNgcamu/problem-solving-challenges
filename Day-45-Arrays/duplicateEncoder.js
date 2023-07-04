84303fd61c9a6d1fcd25c47e74f4286deabf46eb
PhilaNgcamu <philangcamu17@gmail.com>
2023-07-04 16:32:00 +0200

    Duplicate Encoder

--- Day-45-Arrays/duplicateEncoder.js
+++ Day-45-Arrays/duplicateEncoder.js
@@ -1,3 +1,23 @@
 // Codewars > Duplicate Encoder
 
-function duplicateEncode(word) {}
+function duplicateEncode(word) {
+  const lowercasedWord = word.toLowerCase();
+  //have an object to store letters with their occurances
+  const letters = {};
+  //have a for loop on the word
+  //--> if letter exists increment by 1, else assign 1 to then letter on that object
+  for (let i = 0; i < lowercasedWord.length; i++) {
+    letters[lowercasedWord[i]] = (letters[lowercasedWord[i]] || 0) + 1;
+  }
+
+  let result = "";
+  //Have for loop on the lowercasedWord
+  //--> if letter in lowercasedWord is > 1 then assign ")", else "("
+  for (let i = 0; i < lowercasedWord.length; i++) {
+    if (letters[lowercasedWord[i]] === 1) result += "(";
+    else result += ")";
+  }
+  return result;
+}
+
+console.log(duplicateEncode("j"));
