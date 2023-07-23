06962ac8deaade546fd83c1fa982d41858f8182a
PhilaNgcamu <philangcamu17@gmail.com>
2023-07-23 14:51:39 +0200

    Array Plus Arrya

---
+++ Day-47-Debugging/arrayPlusArray.js
@@ -0,0 +1,17 @@
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// function arrayPlusArray(arr1, arr2) {
+//     return arr1 + arr2; //something went wrong
+//   }
+
+//1.How can we restate the problem, in your own words?
+
+//2. What are the inputs that go into the problem?
+
+//3. What should the output look like? (Do we understand how the solution would look like?)
+
+//4. Break down the problem (psedocode)
+//--do we have any errors on our given code? How do we fix that?
+//--then code (if the problem is difficult, let's come up with examples to solidify our understanding)
+
+//5. refactor the solved problem (if possible)
