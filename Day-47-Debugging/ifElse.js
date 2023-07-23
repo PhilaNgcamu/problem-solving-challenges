ad554247ced1b85011acebe81bcdbdad69087ceb
PhilaNgcamu <philangcamu17@gmail.com>
2023-07-23 14:37:54 +0200

    If else challenges

--- Day-47-Debugging/ifElse.js
+++ Day-47-Debugging/ifElse.js
@@ -4,22 +4,22 @@
 
 // The function receives one parameter health which will always be a whole number between -10 and 10.
 
-function checkAlive (health) {
-    if () {
-      health < 0
-      return false
-    } else () {
-      return true
-    }
-  }
-  //1.How can we restate the problem, in your own words?
-  
-  //2. What are the inputs that go into the problem?
-  
-  //3. What should the output look like? (Do we understand how the solution would look like?)
-  
-  //4. Break down the problem (psedocode)
-  //--do we have any errors on our given code?
-  //--then code (if the problem is difficult, let's come up with examples to solidify our understanding)
-  
-  //5. refactor the solved problem (if possible)
+// function checkAlive (health) {
+//     if () {
+//       health < 0
+//       return false
+//     } else () {
+//       return true
+//     }
+//   }
+//1.How can we restate the problem, in your own words?
+
+//2. What are the inputs that go into the problem?
+
+//3. What should the output look like? (Do we understand how the solution would look like?)
+
+//4. Break down the problem (psedocode)
+//--do we have any errors on our given code?
+//--then code (if the problem is difficult, let's come up with examples to solidify our understanding)
+
+//5. refactor the solved problem (if possible)
