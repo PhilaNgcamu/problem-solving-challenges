3239cf93d88cc45e8530ae5740a4b220bc29dda8
PhilaNgcamu <philangcamu17@gmail.com>
2023-07-23 15:00:43 +0200

    Unexpected Parsing

---
+++ Day-47-Debugging/unexpectedParsing.js
@@ -0,0 +1,23 @@
+//Here's a pice of code:
+// function getStatus(isBusy) {
+//     var msg = (isBusy ? "busy" : "available");
+//     return
+//     {
+//       status: msg
+//     }
+//   }
+
+// Expected Behaviour:
+// Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument 'is_busy'.
+
+//1.How can we restate the problem, in your own words?
+
+//2. What are the inputs that go into the problem?
+
+//3. What should the output look like? (Do we understand how the solution would look like?)
+
+//4. Break down the problem (psedocode)
+//--do we have any errors on our given code? How do we fix that?
+//--then code (if the problem is difficult, let's come up with examples to solidify our understanding)
+
+//5. refactor the solved problem (if possible)
