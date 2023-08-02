dde97396e7bd805833ae7d03e7f0c72c6700131f
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-02 11:53:55 +0200

    Simple Magic Square

--- Day-49/magicSquare1.js
+++ Day-49/magicSquare1.js
@@ -13,10 +13,12 @@ const isMagicSquare = (matrix) => {
     if (currentSum !== sum) return false;
   }
   //check for each diagonal
-  for (let i = 0; i < matrix.length; i++) {
-    const currentSum = matrix[i][0] + matrix[i][1] + matrix[i][2];
-    if (currentSum !== sum) return false;
-  }
+  if (
+    matrix[0][0] + matrix[1][1] + matrix[2][2] !== sum ||
+    matrix[0][2] + matrix[1][1] + matrix[2][0] !== sum
+  )
+    return false;
+
   return isMatrix;
 };
 console.log(
