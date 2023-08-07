d00e43017a4128b4866a2e8b0c8704ab04b1c483
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-07 13:55:15 +0200

    Formatted Division

---
+++ Day-50/formattedDivision.js
@@ -0,0 +1,6 @@
+// For this challenge you will divide two numbers and print them in a certain format.
+// have the function FormattedDivision(num1,num2) take both parameters being passed, divide num1 by num2, and return the result as a string with properly formatted commas and 4 significant digits after the decimal place. For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345.6789". The output must contain a number in the one's place even if it is a zero.
+
+function formattedDivision(num1, num2) {
+  return;
+}
