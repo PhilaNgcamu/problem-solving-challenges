7fa66fb9919b0920b10889bc6a4b77e108f2091b
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-07 13:41:34 +0200

    Consecutive

--- Day-50/README.md
+++ Day-50/README.md
@@ -1,3 +1,15 @@
 # Problem Solving: Day 50 - Practicing Coderbyte challenges
 
 Solved 0 problems this day.
+
+How to solve any problem:
+
+1. How can we restate the problem, in your own words?
+
+2. What are the inputs that go into the problem?
+
+3. What should the output look like from the inputs we have? (Do we understand how the solution would look like?)
+
+4. Break down the problem (pseudocode)
+
+5. refactor the solved problem (if possible)
---
+++ Day-50/consecutive.js
@@ -0,0 +1,18 @@
+// For this challenge you will determine how to order an array of numbers consecutively.
+// have the function Consecutive(arr) take the array of integers stored in arr and return the minimum number of integers needed to make the contents of arr consecutive from the lowest number to the highest number. For example: If arr contains [4, 8, 6] then the output should be 2 because two numbers need to be added to the array (5 and 7) to make it a consecutive array of numbers from 4 to 8. Negative numbers may be entered as parameters and no array will have less than 2 elements.
+
+function consecutive(arr) {
+  return arr;
+}
+
+console.log(consecutive([4, 8, 6]));
+
+// 1. How can we restate the problem, in your own words?
+
+// 2. What are the inputs that go into the problem?
+
+// 3. What should the output look like from the inputs we have? (Do we understand how the solution would look like?)
+
+// 4. Break down the problem (pseudocode)
+
+// 5. refactor the solved problem (if possible)
