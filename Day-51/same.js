27d2b6d9a03387cd3e47cb50f6bb78ab2530d88e
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-21 10:25:38 +0200

    Intro To Problem Solving Patterns

---
+++ Day-51/same.js
@@ -0,0 +1,15 @@
+//Write a function called same, which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+
+// 1. Naive approach - where program takes O(n^2) time because of the indexOf method and the for loop
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) return false;
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+// 2. Refactored - Time Complexity - O(n)
+console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
