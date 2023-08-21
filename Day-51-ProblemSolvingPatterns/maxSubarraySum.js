4beb7bc5bd8952e3ffcf61033deebbc05a1b967d
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-21 15:04:38 +0200

    Max Subarray Sum

--- Day-51-ProblemSolvingPatterns/maxSubarraySum.js
+++ Day-51-ProblemSolvingPatterns/maxSubarraySum.js
@@ -18,4 +18,20 @@ function maxSubarraySum(arr, num) {
   return max;
 }
 
+// 2. Refactor - Time complexity O(N)
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
