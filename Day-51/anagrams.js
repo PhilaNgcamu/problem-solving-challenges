87ec73cf0889448815b2d7782dd067bd7ab5ab78
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-21 12:44:38 +0200

    Anagram

---
+++ Day-51/anagrams.js
@@ -0,0 +1,12 @@
+// Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by a rearranging the letters of another, such as cinema, formed from iceman.
+
+// 1. Naive approach
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  for (let i = 0; i < str2.length; i++) {
+    if (!str1.includes(str2[i])) return false;
+  }
+  return true;
+}
+
+console.log(validAnagram("cinema", "iceman"));
