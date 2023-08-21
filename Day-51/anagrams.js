d046f1728827640d394c091fd1b892221e90dc82
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-21 12:56:40 +0200

    Anagrams

--- Day-51/anagrams.js
+++ Day-51/anagrams.js
@@ -9,4 +9,20 @@ function validAnagram(str1, str2) {
   return true;
 }
 
+// 2. Correct approach using frequency counter pattern
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const lookup = {};
+  for (let i = 0; i < str1.length; i++) {
+    lookup[str1[i]] = (lookup[str1[i]] || 0) + 1;
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (!lookup[str2[i]]) return false;
+    else lookup[str2[i]] -= 1;
+  }
+  return true;
+}
 console.log(validAnagram("cinema", "iceman"));
