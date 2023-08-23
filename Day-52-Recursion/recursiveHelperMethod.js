787ccc625b0c946ea45231a9ee21e785aaad049f
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-23 13:25:53 +0200

    Recursive Helper Method

--- Day-52-Recursion/recursiveHelperMethod.js
+++ Day-52-Recursion/recursiveHelperMethod.js
@@ -1,7 +1,16 @@
 const result = [];
 
 function collectOddValues(arr) {
-  if (!arr.length) return;
-  else if (arr[0] % 2 !== 0) result.push(arr[0]);
-  result.slice(1);
+  const result = [];
+
+  function helper(helperInput) {
+    if (!helperInput.length) return;
+    if (helperInput[0] % 2 !== 0) result.push(helperInput[0]);
+
+    helper(helperInput.slice(1));
+  }
+  helper(arr);
+  return result;
 }
+
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
