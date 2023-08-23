b200fbd7d3b04efed3f32dbd750394ba69cc0261
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-23 14:57:27 +0200

    Recursion

--- Day-52-Recursion/factorial.js
+++ Day-52-Recursion/factorial.js
@@ -6,9 +6,12 @@ function factorial(num) {
   return total;
 }
 
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
 function factorial(num) {
   if (num <= 1) return 1;
   return num * factorial(num - 1);
 }
-
-factorial(4);
