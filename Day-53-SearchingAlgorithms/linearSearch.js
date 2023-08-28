f6eeca753f2bde8296f40a1893c51883c2118db5
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-28 10:53:50 +0200

    Linear Search

--- Day-53-SearchingAlgorithms/linearSearch.js
+++ Day-53-SearchingAlgorithms/linearSearch.js
@@ -59,3 +59,16 @@ const states = [
   "Wisconsin",
   "Wyoming",
 ];
+
+// Linear Search Pseudocode:
+// This function accepts an array and a value.
+// Loop through the array and check if the current element is equal to the value.
+// If it is, return the index at which the element is found
+// If the value is never found, return -1
+function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (val === arr[i]) return i;
+  }
+  return -1;
+}
+console.log(linearSearch(states, "Palau"));
