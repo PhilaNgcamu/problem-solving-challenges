bed125e478daf31d5887b8f8b935d5f5862b6cf8
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-28 14:52:49 +0200

    String Search

--- Day-53-SearchingAlgorithms/stringSearch.js
+++ Day-53-SearchingAlgorithms/stringSearch.js
@@ -4,3 +4,17 @@
 // If the characters do match, keep going
 // If you complete the inner loop and find a match, increment the count of matches
 // Return the count
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) {
+        count++; //the number of short found
+      }
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch("lorie loled lolud", "lol"));
