f649bff029bae3566d9c0dee272fd9dd5353c737
PhilaNgcamu <philangcamu17@gmail.com>
2023-08-28 14:37:48 +0200

    String Search

---
+++ Day-53-SearchingAlgorithms/stringSearch.js
@@ -0,0 +1,6 @@
+// string Search Pseudocode:
+// Loop over the longer string
+// If the characters don't match, break out of the inner loop
+// If the characters do match, keep going
+// If you complete the inner loop and find a match, increment the count of matches
+// Return the count
