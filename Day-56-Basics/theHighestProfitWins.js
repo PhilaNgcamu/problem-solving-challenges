eaada8d751e6251cb4e0abf894cf43986806bc51
PhilaNgcamu <philangcamu17@gmail.com>
2023-10-25 15:10:18 +0200

    The Highest Profit Wins

---
+++ Day-56-Basics/theHighestProfitWins.js
@@ -0,0 +1,12 @@
+function minMax(arr) {
+  return [Math.min(...arr), Math.max(...arr)];
+}
+//1. How can we restate the problem, in your own words?
+
+// 2. What are the inputs that go into the problem?
+
+// 3. What should the output look like from the inputs we have? (Do we understand how the solution would look like?)
+
+// 4. Break down the problem (pseudocode)
+
+// 5. refactor the solved problem (if possible)
