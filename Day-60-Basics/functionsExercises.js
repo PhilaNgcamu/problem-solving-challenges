296366412b49b07fe27362f3fbe33d038390854b
PhilaNgcamu <philangcamu17@gmail.com>
2023-11-06 10:46:27 +0200

    Function Basics

--- Day-60-Basics/functionsExercises.js
+++ Day-60-Basics/functionsExercises.js
@@ -1,45 +1,32 @@
 function factorial(input) {
-  if (input === 1) {
-    console.log(input);
-    return;
-  }
-  console.log(input);
-  factorial(input - 1);
+  if (input === 1 || input === 0) return 1;
+  return input * factorial(input - 1);
 }
-function factorial2(input) {
-  console.log(input);
-  factorial3(input - 1);
-}
-function factorial3(input) {
-  console.log(input);
-  factorial4(input - 1);
-}
-function factorial4(input) {
-  console.log(input);
-  factorial5(input - 1);
-}
-function factorial5(input) {
-  console.log(input);
-  factorial6(input - 1);
-}
-function factorial6(input) {
-  console.log(input);
-  factorial7(input - 1);
-}
-function factorial7(input) {
-  console.log(input);
-  factorial8(input - 1);
-}
-function factorial8(input) {
-  console.log(input);
-  factorial9(input - 1);
-}
-function factorial9(input) {
-  console.log(input);
-  factorial10(input - 1);
-}
-function factorial10(input) {
-  console.log(input);
-}
-
-factorial(10);
+// function factorial2(input) {
+//   return input * factorial3(input - 1);
+// }
+// function factorial3(input) {
+//   return input * factorial4(input - 1);
+// }
+// function factorial4(input) {
+//   return input * factorial5(input - 1);
+// }
+// function factorial5(input) {
+//   return input * factorial6(input - 1);
+// }
+// function factorial6(input) {
+//   return input * factorial7(input - 1);
+// }
+// function factorial7(input) {
+//   return input * factorial8(input - 1);
+// }
+// function factorial8(input) {
+//   return input * factorial9(input - 1);
+// }
+// function factorial9(input) {
+//   return input * factorial10(input - 1);
+// }
+// function factorial10(input) {
+//   return input;
+// }
+console.log(factorial(10));
