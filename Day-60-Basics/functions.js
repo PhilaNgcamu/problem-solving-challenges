0de337400a5982f6fbe38c1027d5d72651315412
PhilaNgcamu <philangcamu17@gmail.com>
2023-11-06 09:51:48 +0200

    Function Basics

--- Day-60-Basics/functions.js
+++ Day-60-Basics/functions.js
@@ -1,6 +1,15 @@
 // A function is a set of statements/procedure to perform certain tasks. It should have a relationship between an input and the output
-function fn(number) {
-  return number * number;
+function fn(obj) {
+  return {
+    ...obj,
+    make: "Toyota",
+  };
 }
 
-console.log(fn(6));
+const carObj = {
+  make: "Honda",
+  model: "Accord",
+  year: 1998,
+};
+
+console.log(fn(carObj));
