10f87f0091e7a0362c43c0e346570a71dcecb523
PhilaNgcamu <philangcamu17@gmail.com>
2023-11-06 09:47:01 +0200

    Functions

---
+++ Day-60-Basics/functions.js
@@ -0,0 +1,6 @@
+// A function is a set of statements/procedure to perform certain tasks. It should have a relationship between an input and the output
+function fn(number) {
+  return number * number;
+}
+
+console.log(fn(6));
