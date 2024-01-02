6b7f658916ccbee898d2b44fcb8bde7bcb673c22
PhilaNgcamu <philangcamu17@gmail.com>
2024-01-02 15:04:37 +0200

    Math Basics

---
+++ Day-63/mathOperations.js
@@ -0,0 +1,11 @@
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else if (operation === "/" && value1 !== 0 && value2 !== 0) {
+    return value1 / value2;
+  }
+}
