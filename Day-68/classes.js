760dbe0d5221d4c6804317ade184f844be18904a
PhilaNgcamu <philangcamu17@gmail.com>
2024-02-06 18:25:54 +0200

    Getters and Setters

--- Day-68/classes.js
+++ Day-68/classes.js
@@ -8,8 +8,13 @@ class Person {
   }
 
   set name(newName) {
-    this._name = newName;
+    if (typeof newName === "string") {
+      this._name = newName;
+    } else {
+      console.log("The name must be a string");
+    }
   }
+
   displayInfo() {
     return `My name is ${this._name} and I'm ${this._age} years old`;
   }
@@ -19,5 +24,5 @@ const person = new Person("Phila", 33);
 
 // person.name("Ngcamu");
 // console.log(person.name());
-person.name = "Sande";
+person.name = true;
 console.log(person.name);
