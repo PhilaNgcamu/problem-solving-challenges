8a0e534856d9c0772fc10ebf4ba5b4ad001e42ea
PhilaNgcamu <philangcamu17@gmail.com>
2024-02-06 17:11:26 +0200

    String Basics

--- Day-67-Basics/stringBasics.js
+++ Day-67-Basics/stringBasics.js
@@ -14,18 +14,25 @@
 
 // console.log(string());
 
+// function string() {
+//   const sentence = "The quick brown fox jumps over the lazy dog.";
+
+//   let length = sentence.length;
+
+//   let i = -1;
+//   let iterationCount = 0;
+//   while (iterationCount !== length - 1) {
+//     iterationCount++;
+//     i--;
+//   }
+//   return sentence[iterationCount];
+// }
+
 function string() {
   const sentence = "The quick brown fox jumps over the lazy dog.";
 
-  let length = sentence.length;
-
-  let i = -1;
-  let iterationCount = 0;
-  while (sentence.at(i) !== "0" && iterationCount !== length - 1) {
-    iterationCount++;
-    i--;
-  }
-  return iterationCount;
+  for (let i = 0; i < sentence.length; i++) {}
+  return sentence[iterationCount];
 }
 
 console.log(string());
