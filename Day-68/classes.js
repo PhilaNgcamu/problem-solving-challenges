3dd9b168706cfa342cff285aa10f3a41fe7754fe
PhilaNgcamu <philangcamu17@gmail.com>
2024-02-06 18:04:31 +0200

    Getters and Setters

--- Day-68/classes.js
+++ Day-68/classes.js
@@ -1,6 +1,23 @@
-class MyClass {
-  constructor(parameter1, parameter2) {
-    this.property1 = parameter1;
-    this.property2 = parameter2;
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+  displayInfo() {
+    return `My name is ${this._name} and I'm ${this._age} years old`;
   }
 }
+
+const person = new Person("Phila", 33);
+
+// person.name("Ngcamu");
+// console.log(person.name());
+person.name = "Sande";
+console.log(person.name);
