fd97a17883cc203ce200411463ce09f283d2e49e
PhilaNgcamu <philangcamu17@gmail.com>
2024-02-22 11:38:09 +0200

    Callbacks

--- Day-70/callbacks.js
+++ Day-70/callbacks.js
@@ -1,12 +1,44 @@
 // cashier
-// the on we to call controller
+// there on we to call controller
 // you're at till
 // converse between me and cashier
 // array of ["hello","add items"
 // "void", "smartshopper", "plastic", "have a nice day"]
 // ----------
 // ["soap", "tomato"]
+// remove "soap" from the basket
 // come input the pin when they call him/her;
 
-function cashier() {}
-function controller() {}
+const controllerArr = [
+  "hello",
+  "add items",
+  "void",
+  "smartshopper",
+  "plastic",
+  "have a nice day",
+];
+
+const items = ["soap", "tomato"];
+
+function cashier(arr) {
+  arr.forEach((action, idx) => {
+    console.log(`Cashier: ${action}`);
+    if (action === "add items") {
+      items.push("eggs");
+    } else if (action === "void") {
+      console.log(arr.pop());
+      controller();
+    }
+  });
+}
+
+function controller(arr, idx) {
+  if (arr[idx] === "smartshopper") {
+    console.log("Swipe the discount card (shopper)");
+  } else if (arr[idx] === "plastic") {
+    console.log("Cashier: Do you need a plastic?");
+    console.log("Yes, please");
+  } else "have a nice day";
+}
+
+cashier(controllerArr);
