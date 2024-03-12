9bd6bbe935d4e352a23f35972c17aa54c7b4329f
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-12 13:51:08 +0200

    Fibonacci

--- Week-1/Day-1/recursion.js
+++ Week-1/Day-1/recursion.js
@@ -8,4 +8,14 @@ function factorial(n) {
 
 // 10 * (9 * (8 * (7 * (6 * (5 * (4 * (3 * (2 * 1)))))))) = 3628800
 
-console.log(factorial(10));
+//console.log(factorial(10));
+
+//Exercise: write the function fib(3) where 3 is the nth Fibonacci number
+function fib(n) {
+  if (n <= 1) return 1;
+  else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+//console.log(fib(4));
