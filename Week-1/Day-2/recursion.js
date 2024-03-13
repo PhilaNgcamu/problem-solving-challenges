db96257d0db519db1e25477bf2c70c7fbd778eca
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-13 13:17:25 +0200

    Recursion

--- Week-1/Day-2/recursion.js
+++ Week-1/Day-2/recursion.js
@@ -4,4 +4,15 @@ function reverseString(str) {
   return reverseString(str.slice(1)) + str[0];
 }
 
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
+
+// Example: Palindrome recursively
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  else return str[0] === str[str.length - 1] && isPalindrome(str.slice(1, -1));
+}
+
+// console.log(isPalindrome("madam"));
+// console.log(isPalindrome("hello"));
+// console.log(isPalindrome("racecar"));
