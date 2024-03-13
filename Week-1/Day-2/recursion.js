32a1ef1e06bfd2e3779e0db8c16711a406f8bd7a
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-13 12:54:21 +0200

    Recursion

---
+++ Week-1/Day-2/recursion.js
@@ -0,0 +1,7 @@
+// Example: Reverse a string recursively
+function reverseString(str) {
+  if (str.length === 1) return str;
+  else return str[str.length - 1] + reverseString(str.slice(0, -1));
+}
+
+console.log(reverseString("hello"));
