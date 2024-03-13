a69fe1e5111e024d99dacd14fdeedb9f7f8d6f0f
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-13 13:50:15 +0200

    Recursion

--- Week-1/Day-2/recursion.js
+++ Week-1/Day-2/recursion.js
@@ -17,3 +17,12 @@ function isPalindrome(str, start = 0, end = str.length - 1) {
 // console.log(isPalindrome("madam"));
 // console.log(isPalindrome("hello"));
 // console.log(isPalindrome("racecar"));
+
+// Example: sum of digits recursively
+
+function sumOfDigits(num) {
+  if (num === 0) return num;
+  return (num % 10) + sumOfDigits(Math.floor(num / 10));
+}
+
+console.log(sumOfDigits(12345));
