bfc59c27d7b5328be880cb5cad826a68c7753673
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-19 10:29:17 +0200

    Matrices

---
+++ Week-2/Day-1/matrices2.js
@@ -0,0 +1,26 @@
+A = [
+  [1, 2],
+  [3, 4],
+];
+
+B = [
+  [5, 6],
+  [7, 8],
+];
+
+function matrixAddition(A, B) {
+  const rows = A.length;
+  const cols = A[0].length;
+
+  let result = [];
+  for (let i = 0; i < rows; i++) {
+    result[i] = [];
+    for (let j = 0; j < cols; j++) {
+      result[i][j] = A[i][j] + B[i][j];
+    }
+  }
+  return result;
+}
+
+console.log(matrixAddition(A, B));
+// []
