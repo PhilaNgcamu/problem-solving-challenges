d675559a2f4d7b743e8664c64495d1e49b3c1637
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-19 05:23:41 +0200

    Dynamic Programming

--- Week-2/Day-1/dynamicProgramming.js
+++ Week-2/Day-1/dynamicProgramming.js
@@ -1,8 +1,11 @@
 function fib(n, memo) {
   if (memo[n]) return memo[n];
   if (n <= 2) return 1;
-  var res = fib(n - 1, memo) + fib(n - 2, memo);
-  return (memo[n] = res);
+  else {
+    let res = fib(n - 1, memo) + fib(n - 2, memo);
+    memo[n] = res;
+    return memo[n];
+  }
 }
 
 console.log(fib(5, []));
