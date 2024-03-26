a098d2e670fe82171d24bef003142063a20b74bf
PhilaNgcamu <philangcamu17@gmail.com>
2024-03-27 00:16:15 +0200

    Rotate Matrix

--- Week-3/Day-1/rotateMatrix.js
+++ Week-3/Day-1/rotateMatrix.js
@@ -5,8 +5,14 @@ var rotateMatrix = (arr) => {
     var sub = [];
     for (var j = 0; j < arr.length; j++) {
       sub.push(arr[j][i]);
+      console.log(sub);
     }
     rotated.push(sub);
   }
   return rotated;
 };
+
+rotateMatrix([
+  [-1, 4, 5],
+  [2, 3, 4],
+]);
