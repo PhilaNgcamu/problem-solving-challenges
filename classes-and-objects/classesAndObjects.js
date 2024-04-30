a5c42f6b19dfde55484b24a3c20b201fa0a5efc2
PhilaNgcamu <philangcamu17@gmail.com>
2024-04-30 10:35:40 +0200

    Classes and objects

---
+++ classes-and-objects/classesAndObjects.js
@@ -0,0 +1,28 @@
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === "string") {
+      this._name = newName;
+    } else {
+      console.log("The name must be a string");
+    }
+  }
+
+  displayInfo() {
+    return `My name is ${this._name} and I'm ${this._age} years old`;
+  }
+}
+
+const person = new Person("Phila", 33);
+
+//person.name = 55;
+// console.log(person.name());
+
+console.log(person.displayInfo());
